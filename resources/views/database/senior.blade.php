c6e6e1c36850d768fc682b15cb8434a736c4ec19
addminabhijeet <addmin.abhijeet@gmail.com>
2025-09-30 06:12:50 +0530

    m

--- resources/views/database/senior.blade.php
+++ resources/views/database/senior.blade.php
@@ -140,8 +140,9 @@ $script ='<script>
                         {{-- Amount --}}
                         <td>
                             <input type="text" class="form-control amount-input" data-key="Amount"
-                                value="{{ $row->Amount !== null ? '$' . $row->Amount : '' }}" placeholder="$100">
+                                value="{{ $row->Amount !== null ? '$' . number_format($row->Amount, 2) : '' }}" placeholder="$100">
                         </td>
+
 
 
 
