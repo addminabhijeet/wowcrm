d2347f70849be24e496704c70dc01fb4f1039eee
addminabhijeet <addmin.abhijeet@gmail.com>
2025-09-30 05:47:14 +0530

    Revert "m"
    This reverts commit 0c85dddd31016c135ece749dfe66d3fa92680124.

--- resources/views/database/senior.blade.php
+++ resources/views/database/senior.blade.php
@@ -140,9 +140,8 @@ $script ='<script>
                         {{-- Amount --}}
                         <td>
                             <input type="text" class="form-control amount-input" data-key="Amount"
-                                value="{{ $row->Amount ? '$' . $row->Amount : '' }}" placeholder="$100">
+                                value="{{ $row->Amount ? '$' . number_format($row->Amount, 2) : '' }}" placeholder="$100">
                         </td>
-
 
                         {{-- Qualification --}}
                         <td>
