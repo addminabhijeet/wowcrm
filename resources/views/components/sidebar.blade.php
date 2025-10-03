3256807ae2fde251fa33b282e5601f119e917184
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-04 02:59:30 +0530

    m

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -58,6 +58,9 @@
                     <ul class="sidebar-submenu">
                         <li><a href="{{ route('call.reports.junior') }}"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Call Report</a></li>
                     </ul>
+                    <ul class="sidebar-submenu">
+                        <li><a href="{{ route('call.reports.juniormonthly') }}"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Call Report</a></li>
+                    </ul>
                 </li>
 
                 
