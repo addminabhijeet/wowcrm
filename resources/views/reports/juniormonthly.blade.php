ed3634a43807c4489de93af5862773f3e5c59256
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-04 03:18:24 +0530

    m

--- resources/views/reports/juniormonthly.blade.php
+++ resources/views/reports/juniormonthly.blade.php
@@ -434,7 +434,7 @@ $script = '<script>
                             </div>
                             <div>
                                 <small class="text-muted d-block">Total Calls (TC)</small>
-                                <h4 class="fw-bold text-dark mb-0">{{ $StotalCalls }}</h4>
+                                <h4 class="fw-bold text-dark mb-0">{{ $MtotalCalls }}</h4>
                             </div>
                         </div>
                     </div>
@@ -445,7 +445,7 @@ $script = '<script>
                             </div>
                             <div>
                                 <small class="text-muted d-block">Other Calls (OC)</small>
-                                <h4 class="fw-bold text-dark mb-0">{{ $SotherCalls }}</h4>
+                                <h4 class="fw-bold text-dark mb-0">{{ $MotherCalls }}</h4>
                             </div>
                         </div>
                     </div>
@@ -456,7 +456,7 @@ $script = '<script>
                             </div>
                             <div>
                                 <small class="text-muted d-block">Called & Mailed (C&MC)</small>
-                                <h4 class="fw-bold text-dark mb-0">{{ $ScalledAndMailedCalls }}</h4>
+                                <h4 class="fw-bold text-dark mb-0">{{ $McalledAndMailedCalls }}</h4>
                             </div>
                         </div>
                     </div>
