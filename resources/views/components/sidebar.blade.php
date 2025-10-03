105afe352ee666ab29e56c4349651360b4c58d0a
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-04 04:27:53 +0530

    m

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -104,10 +104,10 @@
                         <span>Report</span>
                     </a>
                     <ul class="sidebar-submenu">
-                        <li><a href="call.reports.junior"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Daily Report</a></li>
+                        <li><a href="call.reports.senior"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Daily Report</a></li>
                     </ul>
                     <ul class="sidebar-submenu">
-                        <li><a href="call.reports.juniormonthly"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Monthly Report</a></li>
+                        <li><a href="call.reports.seniormonthly"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Monthly Report</a></li>
                     </ul>
                 </li>
 
