21b6b327c7df157bea358719f855a30560755dd4
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-06 19:12:57 +0530

    m

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -8,7 +8,7 @@
     </button>
 
     <div>
-        <a href="" class="sidebar-logo">
+        <a href="" class="sidebar-logo" style="text-align: center;">
             <img src="{{ asset('assets/images/logo.png') }}" alt="site logo" class="light-logo">
             <img src="{{ asset('assets/images/logo-light.png') }}" alt="site logo" class="dark-logo">
             <img src="{{ asset('assets/images/logo-icon.png') }}" alt="site logo" class="logo-icon">
