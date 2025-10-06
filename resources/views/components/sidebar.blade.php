03f56b075b5ed6a4efcd72f7823485c4ac8ce65e
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-06 18:00:33 +0530

    m

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -110,7 +110,7 @@
                         <li><a href="{{ route('call.reports.seniormonthly') }}"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Monthly Personal Report</a></li>
                     </ul>
                     <ul class="sidebar-submenu">
-                        <li><a href="{{ route('call.reports.alljuniorlist', ['userId' => Auth::id()]) }}"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Monthly Candidate Report</a></li>
+                        <li><a href="{{ route('call.reports.alljuniorlist', ['userId' => Auth::id()]) }}"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Candidate Report</a></li>
                     </ul>
                 </li>
 
