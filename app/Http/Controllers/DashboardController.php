7985c8e68824e14edbc9d51b0a4d625628274344
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-07 17:23:49 +0530

    n

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -101,6 +101,7 @@ class DashboardController extends Controller
 
             $timer = UserTimerLog::create([
                 'user_id'           => $user->id,
+                'login_id'          => $user->id ?? null,
                 'start_time'        => now(),
                 'remaining_seconds' => $workDaySeconds,
                 'status'            => 'running',
