09ce788ebc763f479a2f3b57a908b4424c36866e
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-07 17:08:01 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard/accountant', [DashboardController::class, 'accountant'])->name('dashboard.accountant');
     Route::get('/dashboard/trainer', [DashboardController::class, 'trainer'])->name('dashboard.trainer');
     Route::get('/button/status', [DashboardController::class, 'getButtonStatus'])->name('button.status');
-    Route::post('/start-timer', [DashboardController::class, 'startTimer'])->name('timer.start');
+    Route::post('/dashboard/start-timer', [DashboardController::class, 'startTimer'])->name('timer.start');
 
 
     Route::get('/dashboard/admin/calendar/{month?}/{year?}', [CalendarController::class, 'index'])->name('calendar.index');
