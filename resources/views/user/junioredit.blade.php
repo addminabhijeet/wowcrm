0e4f13194c06366ba41854601a6cea69270657d6
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-09 00:31:49 +0530

    m

--- resources/views/user/junioredit.blade.php
+++ resources/views/user/junioredit.blade.php
@@ -108,7 +108,7 @@ $script ='<script>
                             <h6 class="text-md text-primary-light mb-16">Profile Image</h6>
 
                             <div class="mb-24 mt-16">
-                                <div class="avatar-preview">
+                                <div class="avatar-upload">
                                     <!-- Edit / Upload Button -->
                                     <div class="avatar-edit position-absolute bottom-0 end-0 me-24 mt-16 z-1 cursor-pointer">
                                         <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" hidden>
