89990363272dfef61d6a376ead4ae98b4acc4594
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-08 21:50:12 +0530

    m

--- resources/views/user/junioredit.blade.php
+++ resources/views/user/junioredit.blade.php
@@ -41,51 +41,47 @@ $script ='<script>
 @section('content')
 
 <div class="row gy-4">
+    @foreach($users as $user)
     <div class="col-lg-4">
         <div class="user-grid-card position-relative border radius-16 overflow-hidden bg-base h-100">
             <img src="{{ asset('assets/images/user-grid/user-grid-bg1.png') }}" alt="" class="w-100 object-fit-cover">
             <div class="pb-24 ms-16 mb-24 me-16  mt--100">
                 <div class="text-center border border-top-0 border-start-0 border-end-0">
                     <img src="{{ asset('assets/images/user-grid/user-grid-img14.png') }}" alt="" class="border br-white border-width-2-px w-200-px h-200-px rounded-circle object-fit-cover">
-                    <h6 class="mb-0 mt-16">Jacob Jones</h6>
-                    <span class="text-secondary-light mb-16">ifrandom@gmail.com</span>
+                    <h6 class="mb-0 mt-16">{{ $user->name }}</h6>
+                    <span class="text-secondary-light mb-16">{{ $user->email }}</span>
                 </div>
                 <div class="mt-24">
                     <h6 class="text-xl mb-16">Personal Info</h6>
                     <ul>
                         <li class="d-flex align-items-center gap-1 mb-12">
                             <span class="w-30 text-md fw-semibold text-primary-light">Full Name</span>
-                            <span class="w-70 text-secondary-light fw-medium">: Will Jonto</span>
+                            <span class="w-70 text-secondary-light fw-medium">: {{ $user->name }}</span>
                         </li>
                         <li class="d-flex align-items-center gap-1 mb-12">
                             <span class="w-30 text-md fw-semibold text-primary-light"> Email</span>
-                            <span class="w-70 text-secondary-light fw-medium">: willjontoax@gmail.com</span>
+                            <span class="w-70 text-secondary-light fw-medium">: {{ $user->email }}</span>
                         </li>
                         <li class="d-flex align-items-center gap-1 mb-12">
                             <span class="w-30 text-md fw-semibold text-primary-light"> Phone Number</span>
                             <span class="w-70 text-secondary-light fw-medium">: (1) 2536 2561 2365</span>
                         </li>
                         <li class="d-flex align-items-center gap-1 mb-12">
-                            <span class="w-30 text-md fw-semibold text-primary-light"> Department</span>
-                            <span class="w-70 text-secondary-light fw-medium">: Design</span>
+                            <span class="w-30 text-md fw-semibold text-primary-light"> Role</span>
+                            <span class="w-70 text-secondary-light fw-medium">: {{ $user->role }}</span>
                         </li>
                         <li class="d-flex align-items-center gap-1 mb-12">
                             <span class="w-30 text-md fw-semibold text-primary-light"> Designation</span>
-                            <span class="w-70 text-secondary-light fw-medium">: UI UX Designer</span>
+                            <span class="w-70 text-secondary-light fw-medium">: Caller</span>
                         </li>
-                        <li class="d-flex align-items-center gap-1 mb-12">
-                            <span class="w-30 text-md fw-semibold text-primary-light"> Languages</span>
-                            <span class="w-70 text-secondary-light fw-medium">: English</span>
-                        </li>
-                        <li class="d-flex align-items-center gap-1">
-                            <span class="w-30 text-md fw-semibold text-primary-light"> Bio</span>
-                            <span class="w-70 text-secondary-light fw-medium">: Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
-                        </li>
+
                     </ul>
                 </div>
             </div>
         </div>
     </div>
+    @endforeach
+
     <div class="col-lg-8">
         <div class="card h-100">
             <div class="card-body p-24">
@@ -126,18 +122,20 @@ $script ='<script>
                             </div>
                         </div>
                         <!-- Upload Image End -->
-                        <form action="#">
+                        <form action="{{ route('users.junior.update', $user->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="mb-20">
                                         <label for="name" class="form-label fw-semibold text-primary-light text-sm mb-8">Full Name <span class="text-danger-600">*</span></label>
-                                        <input type="text" class="form-control radius-8" id="name" placeholder="Enter Full Name">
+                                        <input type="text" class="form-control radius-8" id="name" value="{{ $user->name }}" placeholder="Enter Full Name">
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="mb-20">
                                         <label for="email" class="form-label fw-semibold text-primary-light text-sm mb-8">Email <span class="text-danger-600">*</span></label>
-                                        <input type="email" class="form-control radius-8" id="email" placeholder="Enter email address">
+                                        <input type="email" class="form-control radius-8" id="email" value="{{ $user->email }}" placeholder="Enter email address">
                                     </div>
                                 </div>
                                 <div class="col-sm-6">
@@ -148,7 +146,7 @@ $script ='<script>
                                 </div>
                                 <div class="col-sm-6">
                                     <div class="mb-20">
-                                        <label for="depart" class="form-label fw-semibold text-primary-light text-sm mb-8">Department <span class="text-danger-600">*</span> </label>
+                                        <label for="depart" class="form-label fw-semibold text-primary-light text-sm mb-8">Role <span class="text-danger-600">*</span> </label>
                                         <select class="form-control radius-8 form-select" id="depart">
                                             <option>Enter Event Title </option>
                                             <option>Enter Event Title One </option>
