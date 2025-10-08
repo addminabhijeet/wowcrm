77471e7f15b41a558a56b0e8ec8b3f580d7a07a4
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-09 00:26:44 +0530

    m

--- resources/views/user/junioredit.blade.php
+++ resources/views/user/junioredit.blade.php
@@ -3,7 +3,21 @@
 $title='View Profile';
 $subTitle = 'View Profile';
 $script ='<script>
-
+    // ======================== Upload Image Start =====================
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $("#imagePreview").css("background-image", "url(" + e.target.result + ")");
+                $("#imagePreview").hide();
+                $("#imagePreview").fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(function() {
+        readURL(this);
+    });
     // ================== Password Show Hide Js Start ==========
     function initializePasswordToggle(toggleSelector) {
         $(toggleSelector).on("click", function() {
