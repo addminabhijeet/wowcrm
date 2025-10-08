648ac9fa4bea1196f68a20dc0e64494383919c47
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-08 17:22:21 +0530

    m

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,40 +7,39 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-
     public function index()
     {
-        $users = User::all();
+        $users = User::where('role', 'admin')->get();
         return view('user.admin', compact('users'));
     }
 
     public function junior()
     {
-        $users = User::all();
+        $users = User::where('role', 'junior')->get();
         return view('user.junior', compact('users'));
     }
 
     public function senior()
     {
-        $users = User::all();
+        $users = User::where('role', 'senior')->get();
         return view('user.senior', compact('users'));
     }
 
     public function trainer()
     {
-        $users = User::all();
+        $users = User::where('role', 'trainer')->get();
         return view('user.trainer', compact('users'));
     }
 
     public function accountant()
     {
-        $users = User::all();
+        $users = User::where('role', 'accountant')->get();
         return view('user.accountant', compact('users'));
     }
 
     public function customer()
     {
-        $users = User::all();
+        $users = User::where('role', 'customer')->get();
         return view('user.customer', compact('users'));
     }
 }
