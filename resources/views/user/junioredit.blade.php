6fa41ef58174a3784e04b669bb6d7d50634a4bcd
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-08 23:43:19 +0530

    m

--- resources/views/user/junioredit.blade.php
+++ resources/views/user/junioredit.blade.php
@@ -63,7 +63,7 @@ $script ='<script>
                         </li>
                         <li class="d-flex align-items-center gap-1 mb-12">
                             <span class="w-30 text-md fw-semibold text-primary-light"> Phone Number</span>
-                            <span class="w-70 text-secondary-light fw-medium">: {{ $user->email }}</span>
+                            <span class="w-70 text-secondary-light fw-medium">: {{ $user->phone }}</span>
                         </li>
                         <li class="d-flex align-items-center gap-1 mb-12">
                             <span class="w-30 text-md fw-semibold text-primary-light"> Role</span>
@@ -84,6 +84,7 @@ $script ='<script>
     <div class="col-lg-8">
         <div class="card h-100">
             <div class="card-body p-24">
+
                 <ul class="nav border-gradient-tab nav-pills mb-20 d-inline-flex" id="pills-tab" role="tablist">
                     <li class="nav-item" role="presentation">
                         <button class="nav-link d-flex align-items-center px-24 active" id="pills-edit-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-edit-profile" type="button" role="tab" aria-controls="pills-edit-profile" aria-selected="true">
@@ -101,29 +102,29 @@ $script ='<script>
                         </button>
                     </li>
                 </ul>
-
-                <div class="tab-content" id="pills-tabContent">
-                    <div class="tab-pane fade show active" id="pills-edit-profile" role="tabpanel" aria-labelledby="pills-edit-profile-tab" tabindex="0">
-                        <h6 class="text-md text-primary-light mb-16">Profile Image</h6>
-                        <!-- Upload Image Start -->
-                        <div class="mb-24 mt-16">
-                            <div class="avatar-upload">
-                                <div class="avatar-edit position-absolute bottom-0 end-0 me-24 mt-16 z-1 cursor-pointer">
-                                    <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" hidden>
-                                    <label for="imageUpload" class="w-32-px h-32-px d-flex justify-content-center align-items-center bg-primary-50 text-primary-600 border border-primary-600 bg-hover-primary-100 text-lg rounded-circle">
-                                        <iconify-icon icon="solar:camera-outline" class="icon"></iconify-icon>
-                                    </label>
-                                </div>
-                                <div class="avatar-preview">
-                                    <div id="imagePreview">
+                <form action="{{ route('users.junior.update', $user->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active" id="pills-edit-profile" role="tabpanel" aria-labelledby="pills-edit-profile-tab" tabindex="0">
+                            <h6 class="text-md text-primary-light mb-16">Profile Image</h6>
+                            <!-- Upload Image Start -->
+                            <div class="mb-24 mt-16">
+                                <div class="avatar-upload">
+                                    <div class="avatar-edit position-absolute bottom-0 end-0 me-24 mt-16 z-1 cursor-pointer">
+                                        <input type='file' name="image" id="imageUpload" accept=".png, .jpg, .jpeg" hidden>
+                                        <label for="imageUpload" class="w-32-px h-32-px d-flex justify-content-center align-items-center bg-primary-50 text-primary-600 border border-primary-600 bg-hover-primary-100 text-lg rounded-circle">
+                                            <iconify-icon icon="solar:camera-outline" class="icon"></iconify-icon>
+                                        </label>
+                                    </div>
+                                    <div class="avatar-preview">
+                                        <div id="imagePreview">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <!-- Upload Image End -->
-                        <form action="{{ route('users.junior.update', $user->id) }}" method="POST">
-                            @csrf
-                            @method('PUT')
+                            <!-- Upload Image End -->
+
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div class="mb-20">
@@ -175,73 +176,49 @@ $script ='<script>
                                 </div>
 
                                 <div class="d-flex align-items-center justify-content-center gap-3">
-                                    <button type="button" class="border border-danger-600 bg-hover-danger-200 text-danger-600 text-md px-56 py-11 radius-8">
-                                        Cancel
-                                    </button>
-                                    <button type="button" class="btn btn-primary border border-primary-600 text-md px-56 py-12 radius-8">
+                                    <button type="submit" class="btn btn-primary border border-primary-600 text-md px-56 py-12 radius-8">
                                         Save
                                     </button>
                                 </div>
-                            </div>
-                        </form>
-                    </div>
 
-                    <div class="tab-pane fade" id="pills-change-passwork" role="tabpanel" aria-labelledby="pills-change-passwork-tab" tabindex="0">
-                        <div class="mb-20">
-                            <label for="your-password" class="form-label fw-semibold text-primary-light text-sm mb-8">New Password <span class="text-danger-600">*</span></label>
-                            <div class="position-relative">
-                                <input type="password" class="form-control radius-8" id="your-password" placeholder="Enter New Password*">
-                                <span class="toggle-password ri-eye-line cursor-pointer position-absolute end-0 top-50 translate-middle-y me-16 text-secondary-light" data-toggle="#your-password"></span>
                             </div>
-                        </div>
-                        <div class="mb-20">
-                            <label for="confirm-password" class="form-label fw-semibold text-primary-light text-sm mb-8">Confirmed Password <span class="text-danger-600">*</span></label>
-                            <div class="position-relative">
-                                <input type="password" class="form-control radius-8" id="confirm-password" placeholder="Confirm Password*">
-                                <span class="toggle-password ri-eye-line cursor-pointer position-absolute end-0 top-50 translate-middle-y me-16 text-secondary-light" data-toggle="#confirm-password"></span>
-                            </div>
-                        </div>
-                    </div>
 
-                    <div class="tab-pane fade" id="pills-notification" role="tabpanel" aria-labelledby="pills-notification-tab" tabindex="0">
-                        <div class="form-switch switch-primary py-12 px-16 border radius-8 position-relative mb-16">
-                            <label for="companzNew" class="position-absolute w-100 h-100 start-0 top-0"></label>
-                            <div class="d-flex align-items-center gap-3 justify-content-between">
-                                <span class="form-check-label line-height-1 fw-medium text-secondary-light">Company News</span>
-                                <input class="form-check-input" type="checkbox" role="switch" id="companzNew">
-                            </div>
                         </div>
-                        <div class="form-switch switch-primary py-12 px-16 border radius-8 position-relative mb-16">
-                            <label for="pushNotifcation" class="position-absolute w-100 h-100 start-0 top-0"></label>
-                            <div class="d-flex align-items-center gap-3 justify-content-between">
-                                <span class="form-check-label line-height-1 fw-medium text-secondary-light">Push Notification</span>
-                                <input class="form-check-input" type="checkbox" role="switch" id="pushNotifcation" checked>
-                            </div>
-                        </div>
-                        <div class="form-switch switch-primary py-12 px-16 border radius-8 position-relative mb-16">
-                            <label for="weeklyLetters" class="position-absolute w-100 h-100 start-0 top-0"></label>
-                            <div class="d-flex align-items-center gap-3 justify-content-between">
-                                <span class="form-check-label line-height-1 fw-medium text-secondary-light">Weekly News Letters</span>
-                                <input class="form-check-input" type="checkbox" role="switch" id="weeklyLetters" checked>
-                            </div>
-                        </div>
-                        <div class="form-switch switch-primary py-12 px-16 border radius-8 position-relative mb-16">
-                            <label for="meetUp" class="position-absolute w-100 h-100 start-0 top-0"></label>
-                            <div class="d-flex align-items-center gap-3 justify-content-between">
-                                <span class="form-check-label line-height-1 fw-medium text-secondary-light">Meetups Near you</span>
-                                <input class="form-check-input" type="checkbox" role="switch" id="meetUp">
-                            </div>
-                        </div>
-                        <div class="form-switch switch-primary py-12 px-16 border radius-8 position-relative mb-16">
-                            <label for="orderNotification" class="position-absolute w-100 h-100 start-0 top-0"></label>
-                            <div class="d-flex align-items-center gap-3 justify-content-between">
-                                <span class="form-check-label line-height-1 fw-medium text-secondary-light">Orders Notifications</span>
-                                <input class="form-check-input" type="checkbox" role="switch" id="orderNotification" checked>
-                            </div>
-                        </div>
-                    </div>
 
-                </div>
+                        <div class="tab-pane fade" id="pills-change-passwork" role="tabpanel" aria-labelledby="pills-change-passwork-tab" tabindex="0">
+                            <div class="mb-20">
+                                <label for="your-password" class="form-label fw-semibold text-primary-light text-sm mb-8">New Password <span class="text-danger-600">*</span></label>
+                                <div class="position-relative">
+                                    <input type="password" name="password" class="form-control radius-8" id="your-password" placeholder="Enter New Password*">
+
+                                    <span class="toggle-password ri-eye-line cursor-pointer position-absolute end-0 top-50 translate-middle-y me-16 text-secondary-light" data-toggle="#your-password"></span>
+                                </div>
+                            </div>
+                            <div class="mb-20">
+                                <label for="confirm-password" class="form-label fw-semibold text-primary-light text-sm mb-8">Confirmed Password <span class="text-danger-600">*</span></label>
+                                <div class="position-relative">
+                                    <input type="password" name="password_confirmation" class="form-control radius-8" id="confirm-password" placeholder="Confirm Password*">
+                                    <span class="toggle-password ri-eye-line cursor-pointer position-absolute end-0 top-50 translate-middle-y me-16 text-secondary-light" data-toggle="#confirm-password"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="tab-pane fade" id="pills-notification" role="tabpanel" aria-labelledby="pills-notification-tab" tabindex="0">
+                            <div class="form-switch switch-primary py-12 px-16 border radius-8 position-relative mb-16">
+                                <label for="status" class="position-absolute w-100 h-100 start-0 top-0"></label>
+                                <div class="d-flex align-items-center gap-3 justify-content-between">
+                                    <span class="form-check-label line-height-1 fw-medium text-secondary-light">
+                                        Account Status
+                                    </span>
+                                    <input class="form-check-input" type="checkbox" role="switch" id="status" name="status" value="1" {{ $user->status == 1 ? 'checked' : '' }}>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </form>
             </div>
         </div>
     </div>
