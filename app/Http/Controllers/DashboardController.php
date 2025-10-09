2c22170936e133c06ebf374a3dec720f3c2b9f6c
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-09 17:03:16 +0530

    m

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -331,14 +331,6 @@ class DashboardController extends Controller
                 ]);
             }
 
-            UserTimerPause::create([
-                'user_timer_log_id' => $timer->id,
-                'user_id'           => $user->id,
-                'status'            => 'running',
-                'pause_type'        => 'resume',
-                'remaining_seconds' => $timer->remaining_seconds,
-                'event_time'        => now(),
-            ]);
         } elseif (in_array($action, ['lunch', 'tea', 'break'])) {
 
             $pauseLabels = [
