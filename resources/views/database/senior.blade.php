d05636b5dcb8a5b0026d9b38f2a66389668adba9
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-10 04:02:14 +0530

    m

--- resources/views/database/senior.blade.php
+++ resources/views/database/senior.blade.php
@@ -631,13 +631,14 @@ $script ='<script>
                     cells += `<td><input type="email" class="form-control email-input" data-key="${k}" placeholder="Email"><span class="small-hint"></span></td>`;
                 } else if (k === 'Name') {
                     cells += `<td><input type="text" class="form-control name-input" data-key="${k}" placeholder="Name"><span class="small-hint"></span></td>`;
-                } else if (k === 'Forwarded By') {
-    // Automatically fill with logged-in user or leave blank, readonly
+} else if (k === 'Forwarded By') {
     const currentUser = "{{ Auth::user()->name ?? '' }}";
     cells += `<td>
         <input type="text" class="form-control forwardedBy-input" data-key="Forwarded By" value="${currentUser}" placeholder="Forwarded By" readonly>
         <span class="small-hint"></span>
     </td>`;
+
+
 
                 } else if (k === 'View') {
                     cells += `<td>
