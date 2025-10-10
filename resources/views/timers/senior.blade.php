55758f4bbc753e3a7775764a8bf3aacb512ea255
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-10 20:28:51 +0530

    m

--- resources/views/timers/senior.blade.php
+++ resources/views/timers/senior.blade.php
@@ -121,6 +121,9 @@ $script = '<script>
                                 <button data-type="logout" data-user="{{ $timer['user_id'] }}" style="width:65px;height:28px;border-radius:14px;background:#f8f9fa;border:1px solid #ddd;display:flex;align-items:center;justify-content:center;font-size:12px;color:#007bff;">
                                     <iconify-icon icon="mdi:pause" style="margin-right:2px;font-size:14px;"></iconify-icon>Log Out
                                 </button>
+                                <button data-type="logout" data-user="{{ $timer['user_id'] }}"style="width:65px;height:28px;border-radius:14px;background:#dc3545;border:1px solid #b02a37;display:flex;align-items:center;justify-content:center;font-size:12px;color:#fff;cursor:pointer;">
+                                    <iconify-icon icon="mdi:pause" style="margin-right:2px;font-size:14px;"></iconify-icon>Log Out
+                                </button>
                             </div>
                         </div>
                         <!-- Action Buttons -->
