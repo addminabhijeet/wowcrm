0629d22c99111dd84bf5baf762c889e22c56e905
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-13 03:43:33 +0530

    m

--- resources/views/smtp/edit.blade.php
+++ resources/views/smtp/edit.blade.php
@@ -139,13 +139,13 @@ $script = '<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>';
                                 {{-- From Name --}}
                                 <div class="col-sm-6">
                                     <label class="form-label fw-semibold text-primary-light text-md mb-8">
-                                        From Name <span class="text-danger-600">*</span>
+                                        User Name <span class="text-danger-600">*</span>
                                     </label>
                                     <div class="input-group radius-8">
                                         <span class="input-group-text bg-neutral-100 border-neutral-300">
                                             <iconify-icon icon="mdi:account-box-outline"></iconify-icon>
                                         </span>
-                                        <select name="user_id" class="form-control radius-8">
+                                        <select name="user_id" class="form-control radius-8" disabled>
                                             <option value="">Select User</option>
                                             @foreach($users as $user)
                                             <option value="{{ $user->id }}"
