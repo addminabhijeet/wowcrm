d36e53f36fa6b47eda46494841f8859d9f46f770
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-13 04:19:33 +0530

    m

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -506,17 +506,14 @@ class DashboardController extends Controller
 
     public function add()
     {
-        return view('smtp.add');
-    }
-    // Show the form to edit SMTP settings
-    public function edit($userId)
-    {
-        $smtp = SmtpSetting::where('user_id', $userId)->first(); // Get SMTP for specific user
-        $users = User::all(); // Keep for dropdown or selection if needed
+        // Get all user IDs that already have SMTP settings
+        $smtpUserIds = SmtpSetting::pluck('user_id')->toArray();
 
-        return view('smtp.edit', compact('smtp', 'users'));
-    }
+        // Get users who do not have SMTP settings
+        $users = User::whereNotIn('id', $smtpUserIds)->get();
 
+        return view('smtp.add', compact('users'));
+    }
 
 
     // Update SMTP settings
