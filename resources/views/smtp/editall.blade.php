de0ed59344f60cfe20d95737c79e138bfcb8352d
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-13 02:58:55 +0530

    m

--- resources/views/smtp/editall.blade.php
+++ resources/views/smtp/editall.blade.php
@@ -27,6 +27,11 @@ $script = '<script>
                 <option>9</option>
                 <option>10</option>
             </select>
+            <a href="javascript:void(0)" class="btn btn-primary text-sm btn-sm px-12 py-12 radius-8 d-flex align-items-center gap-2"
+                >
+                <iconify-icon icon="ic:baseline-plus" class="icon text-xl line-height-1"></iconify-icon>
+                All SMTP User
+            </a>
             <form class="navbar-search">
                 <input type="text" class="bg-base h-40-px w-auto" name="search" placeholder="Search">
                 <iconify-icon icon="ion:search-outline" class="icon"></iconify-icon>
@@ -64,7 +69,7 @@ $script = '<script>
 
                         <td class="text-center">
                             <a href="{{ route('calendar.alljuniorUser', $user->id) }}" class="btn btn-sm btn-primary">
-                                View 
+                                View
                             </a>
                         </td>
 
