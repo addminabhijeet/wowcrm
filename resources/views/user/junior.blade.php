05f8f007417e9d459735af544ece0b6b6616c810
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-13 13:23:39 +0530

    m

--- resources/views/user/junior.blade.php
+++ resources/views/user/junior.blade.php
@@ -47,7 +47,7 @@ $script= '<script src="' . asset('assets/js/homeOneChart.js') . '"></script>';
                                         <!-- User info (avatar + name + email) -->
                                         <td>
                                             <div class="d-flex align-items-center">
-                                                <img src="{{ $user->image ? asset('storage/user_images/' . $user->image) : asset('assets/images/users/user1.png') }}" alt="{{ $user->name }}" class="w-40-px h-40-px rounded-circle flex-shrink-0 me-12 overflow-hidden">
+                                                <img src="{{ $user->image ? asset('assets/images/user-grid/' . $user->image) : asset('assets/images/users/user1.png') }}" alt="{{ $user->name }}" class="w-40-px h-40-px rounded-circle flex-shrink-0 me-12 overflow-hidden">
                                                 <div class="flex-grow-1">
                                                     <h6 class="text-md mb-0 fw-medium">{{ $user->name }}</h6>
                                                     <span class="text-sm text-secondary-light fw-medium">{{ $user->email }}</span>
