0b80e2b57b2dace92ee3fe492347fd32ce6b3113
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-13 13:12:33 +0530

    m

--- resources/views/dashboard/senior.blade.php
+++ resources/views/dashboard/senior.blade.php
@@ -15,7 +15,7 @@
                             <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                                 <div>
                                     <p class="fw-medium text-primary-light mb-1">Total Resumes</p>
-                                    <h6 class="mb-0">20</h6>
+                                    <h6 class="mb-0">0</h6>
                                 </div>
                                 <div class="w-50-px h-50-px bg-cyan rounded-circle d-flex justify-content-center align-items-center">
                                     <iconify-icon icon="gridicons:multiple-users" class="text-white text-2xl mb-0"></iconify-icon>
@@ -36,7 +36,7 @@
                             <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                                 <div>
                                     <p class="fw-medium text-primary-light mb-1">Total Pending Resumes</p>
-                                    <h6 class="mb-0">10</h6>
+                                    <h6 class="mb-0">0</h6>
                                 </div>
                                 <div class="w-50-px h-50-px bg-purple rounded-circle d-flex justify-content-center align-items-center">
                                     <iconify-icon icon="fa-solid:award" class="text-white text-2xl mb-0"></iconify-icon>
@@ -57,7 +57,7 @@
                             <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                                 <div>
                                     <p class="fw-medium text-primary-light mb-1">Total Inreview Resumes</p>
-                                    <h6 class="mb-0">10</h6>
+                                    <h6 class="mb-0">0</h6>
                                 </div>
                                 <div class="w-50-px h-50-px bg-info rounded-circle d-flex justify-content-center align-items-center">
                                     <iconify-icon icon="fluent:people-20-filled" class="text-white text-2xl mb-0"></iconify-icon>
@@ -78,7 +78,7 @@
                             <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                                 <div>
                                     <p class="fw-medium text-primary-light mb-1">Total Completed Resumes</p>
-                                    <h6 class="mb-0">20</h6>
+                                    <h6 class="mb-0">0</h6>
                                 </div>
                                 <div class="w-50-px h-50-px bg-success-main rounded-circle d-flex justify-content-center align-items-center">
                                     <iconify-icon icon="solar:wallet-bold" class="text-white text-2xl mb-0"></iconify-icon>
