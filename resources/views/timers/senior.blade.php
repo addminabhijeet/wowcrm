535e42379ef52a048dde32a9e4924e77893e3fd7
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-13 17:34:20 +0530

    m

--- resources/views/timers/senior.blade.php
+++ resources/views/timers/senior.blade.php
@@ -65,7 +65,7 @@ $script = '<script>
                     </div>
 
                     <div class="ps-16 pb-16 pe-16 text-center mt--50">
-                        <img src="{{ !empty($timer['image']) && file_exists(public_path('assets/images/user-grid/' . $timer['image'])) ? asset('assets/images/user-grid/' . $timer['image']) : asset('assets/images/user-grid/default-user.png') }}" class="border br-white border-width-2-px w-100-px h-100-px rounded-circle object-fit-cover" alt="">
+                        <img src="{{ $timer['image'] ? asset('assets/images/user-grid/' . $timer['image']) : asset('assets/images/user-grid/user-grid-bg1.png') }}" class="border br-white border-width-2-px w-100-px h-100-px rounded-circle object-fit-cover" alt="">
                         <h6 class="text-lg mb-0 mt-4">{{ $timer['name'] }}</h6>
                         <span class="text-secondary-light mb-16">{{ $timer['email'] }}</span>
 
