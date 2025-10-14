6614f3604f097970ccdefbeddd95fc057ef57cab
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-14 19:03:37 +0530

    m

--- resources/views/reports/junior.blade.php
+++ resources/views/reports/junior.blade.php
@@ -474,58 +474,57 @@ $script = '<script>
                         </thead>
                         <tbody>
                             <tr>
-                                <td>10.00AM-11.00AM</td>
+                                <td style="white-space: nowrap;">10.00AM-11.00AM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t10to11am }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o10to11am }}</span></td>
                             </tr>
                             <tr>
-                                <td>11.00AM-12.00PM</td>
+                                <td style="white-space: nowrap;">11.00AM-12.00PM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t11to12pm }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o11to12pm }}</span></td>
                             </tr>
                             <tr>
-                                <td>12.00PM-01.00PM</td>
+                                <td style="white-space: nowrap;">12.00PM-01.00PM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t12to1pm }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o12to1pm }}</span></td>
                             </tr>
                             <tr>
-                                <td>01:00PM-02.00PM</td>
+                                <td style="white-space: nowrap;">01.00PM-02.00PM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t1to2pm }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o1to2pm }}</span></td>
                             </tr>
                             <tr>
-                                <td>02:00PM-03.00PM</td>
+                                <td style="white-space: nowrap;">02.00PM-03.00PM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t2to3pm }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o2to3pm }}</span></td>
                             </tr>
                             <tr>
-                                <td>03.00PM-04.00PM</td>
+                                <td style="white-space: nowrap;">03.00PM-04.00PM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t3to4pm }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o3to4pm }}</span></td>
                             </tr>
                             <tr>
-                                <td>04.00PM-05.00PM</td>
+                                <td style="white-space: nowrap;">04.00PM-05.00PM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t4to5pm }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o4to5pm }}</span></td>
                             </tr>
                             <tr>
-                                <td>05.00PM-06.00PM</td>
+                                <td style="white-space: nowrap;">05.00PM-06.00PM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t5to6pm }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o5to6pm }}</span></td>
                             </tr>
                             <tr>
-                                <td>06.00PM-07.00PM</td>
+                                <td style="white-space: nowrap;">06.00PM-07.00PM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t6to7pm }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o6to7pm }}</span></td>
                             </tr>
                             <tr>
-                                <td>07.00PM-08.00PM</td>
+                                <td style="white-space: nowrap;">07.00PM-08.00PM</td>
                                 <td class="text-center"><span class="badge bg-info">{{ $t7to8pm }}</span></td>
                                 <td class="text-center"><span class="badge bg-warning">{{ $o7to8pm }}</span></td>
                             </tr>
                         </tbody>
                     </table>
-
                 </div>
             </div>
         </div>
