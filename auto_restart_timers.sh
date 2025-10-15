80288b629b2c44cab372e6a6d015a17b6d653f4c
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-16 03:59:28 +0530

    m

--- auto_restart_timers.sh
+++ auto_restart_timers.sh
@@ -3,13 +3,18 @@
 # Path to your Laravel project
 PROJECT_PATH="/home/u235777426/public_html"
 
-# Full PHP path (from your hosting)
+# Full PHP path from hosting
 PHP_PATH="/usr/bin/php"
 
 # Infinite loop to auto-restart the command if it fails
 while true; do
     echo "[$(date)] Starting timers:decrement process..."
-    $PHP_PATH $PROJECT_PATH/artisan timers:decrement
+    
+    # Go to Laravel directory
+    cd $PROJECT_PATH || exit
+
+    # Run the artisan command
+    $PHP_PATH artisan timers:decrement
 
     echo "[$(date)] Process crashed or stopped. Restarting in 5 seconds..."
     sleep 5
