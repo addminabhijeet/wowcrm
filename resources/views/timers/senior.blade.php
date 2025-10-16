a5321e14838e0a3ef351f736e0f1b5ee24d9e524
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-16 14:28:04 +0530

    m

--- resources/views/timers/senior.blade.php
+++ resources/views/timers/senior.blade.php
@@ -156,12 +156,12 @@ $script = '<script>
 
                         <!-- Label instead of Dropdown -->
                         <div class="position-absolute top-0 end-0 me-16 mt-16">
-                            @if($timer['button_status'] == 0)
+                            @if($timer['pause_type'] == lunch)
                             <span class="badge bg-danger text-white px-3 py-2 radius-8 shadow-sm"
                                 style="font-size:12px; min-width:100px; text-align:center;">
                                 New
                             </span>
-                            @else
+                            @elseif($timer['pause_type'] == 'tea')
                             <span class="badge bg-success text-white px-3 py-2 radius-8 shadow-sm"
                                 style="font-size:12px; min-width:100px; text-align:center;">
                                 New Arrival
