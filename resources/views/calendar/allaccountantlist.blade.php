954fc0b601e50da45922449323303d1b6b20a039
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-23 19:10:49 +0530

    m

--- resources/views/calendar/allaccountantlist.blade.php
+++ resources/views/calendar/allaccountantlist.blade.php
@@ -54,7 +54,7 @@ $script = '<script>
                     </tr>
                 </thead>
                 <tbody>
-                    @forelse($juniorUsers as $index => $user)
+                    @forelse($accountantUsers as $index => $user)
                     <tr>
                         <td>{{ $index + 1 }}</td>
 
