43c582ed36488016b03a3648caee0c4fe3a6e5f3
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-23 17:34:40 +0530

    m

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -253,7 +253,7 @@ $role = Auth::user()->role;
                     <span>Report</span>
                 </a>
                 <ul class="sidebar-submenu">
-                    <li><a href="{{ route('call.reports.allseniorlist') }}"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Monthly Personal Report</a></li>
+                    <li><a href="{{ route('call.reports.allseniorlist') }}"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Senior Report</a></li>
                 </ul>
                 <ul class="sidebar-submenu">
                     <li><a href="{{ route('call.reports.alljuniorlist', ['userId' => Auth::id()]) }}"><i class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Team Report</a></li>
