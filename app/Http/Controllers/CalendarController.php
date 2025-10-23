2b32278aff5945cdaaef28cc97e7bdf1571db651
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-23 18:32:18 +0530

    m

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -11,11 +11,6 @@ use App\Models\User;
 
 class CalendarController extends Controller
 {
-    public function index()
-    {
-        
-        return view('calendar.admin');
-    }
 
     public function juniorUser(Request $request)
     {
