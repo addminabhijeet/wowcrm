701f62181bd28263c751e3d57614554728892456
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-30 04:31:22 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -182,7 +182,7 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/dashboard/target/save/{user}', [DashboardController::class, 'targetSave'])->name('target.save');
     Route::post('/dashboard/target/delete/{id}', [DashboardController::class, 'targetDelete'])->name('target.delete');
     Route::get('/dashboard/target/targetall', [DashboardController::class, 'targetall'])->name('target.all');
-    
+
 
     Route::put('/dashboard/smtp/allupdate', [DashboardController::class, 'addupdate'])->name('smtp.addupdate');
     Route::put('/dashboard/smtp/update/{user}', [DashboardController::class, 'update'])->name('smtp.update');
