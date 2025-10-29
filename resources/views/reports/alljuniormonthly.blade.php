946ae20dd9299cbb1d4ef5cecf2ed9bfb9c1a793
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-29 21:51:00 +0530

    m

--- resources/views/reports/alljuniormonthly.blade.php
+++ resources/views/reports/alljuniormonthly.blade.php
@@ -409,19 +409,13 @@ $script = '<script>
             <div class="card-body p-20">
                 <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                     <div>
-                        <p class="fw-medium text-primary-light mb-1">Total Resumes</p>
+                        <p class="fw-medium text-primary-light mb-1">Total Target Given</p>
                         <h6 class="mb-0">0</h6>
                     </div>
                     <div class="w-50-px h-50-px bg-cyan rounded-circle d-flex justify-content-center align-items-center">
                         <iconify-icon icon="gridicons:multiple-users" class="text-white text-2xl mb-0"></iconify-icon>
                     </div>
                 </div>
-                <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
-                    <span class="d-inline-flex align-items-center gap-1 text-success-main">
-                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> +6
-                    </span>
-                    Last 30 days total resumes
-                </p>
             </div>
         </div><!-- card end -->
     </div>
@@ -430,19 +424,13 @@ $script = '<script>
             <div class="card-body p-20">
                 <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                     <div>
-                        <p class="fw-medium text-primary-light mb-1">Total Pending Resumes</p>
+                        <p class="fw-medium text-primary-light mb-1">Total Target Archived</p>
                         <h6 class="mb-0">0</h6>
                     </div>
                     <div class="w-50-px h-50-px bg-purple rounded-circle d-flex justify-content-center align-items-center">
                         <iconify-icon icon="fa-solid:award" class="text-white text-2xl mb-0"></iconify-icon>
                     </div>
                 </div>
-                <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
-                    <span class="d-inline-flex align-items-center gap-1 text-danger-main">
-                        <iconify-icon icon="bxs:down-arrow" class="text-xs"></iconify-icon> -1
-                    </span>
-                    Last 30 days total pending resumes
-                </p>
             </div>
         </div><!-- card end -->
     </div>
@@ -451,19 +439,13 @@ $script = '<script>
             <div class="card-body p-20">
                 <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                     <div>
-                        <p class="fw-medium text-primary-light mb-1">Total Inreview Resumes</p>
+                        <p class="fw-medium text-primary-light mb-1">Total Target Need To Achived</p>
                         <h6 class="mb-0">0</h6>
                     </div>
                     <div class="w-50-px h-50-px bg-info rounded-circle d-flex justify-content-center align-items-center">
                         <iconify-icon icon="fluent:people-20-filled" class="text-white text-2xl mb-0"></iconify-icon>
                     </div>
                 </div>
-                <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
-                    <span class="d-inline-flex align-items-center gap-1 text-success-main">
-                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> +1
-                    </span>
-                    Last 30 days total inreview resumes
-                </p>
             </div>
         </div><!-- card end -->
     </div>
@@ -472,19 +454,13 @@ $script = '<script>
             <div class="card-body p-20">
                 <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                     <div>
-                        <p class="fw-medium text-primary-light mb-1">Total Completed Resumes</p>
+                        <p class="fw-medium text-primary-light mb-1">Total Target Need To Achived</p>
                         <h6 class="mb-0">0</h6>
                     </div>
                     <div class="w-50-px h-50-px bg-success-main rounded-circle d-flex justify-content-center align-items-center">
                         <iconify-icon icon="solar:wallet-bold" class="text-white text-2xl mb-0"></iconify-icon>
                     </div>
                 </div>
-                <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
-                    <span class="d-inline-flex align-items-center gap-1 text-success-main">
-                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> +$1
-                    </span>
-                    Last 30 days Total completed resumes
-                </p>
             </div>
         </div><!-- card end -->
     </div>
