e7cabcb25669069fe24bcd8be3ddef15e7149577
addminabhijeet <addmin.abhijeet@gmail.com>
2025-10-30 19:56:59 +0530

    m

--- app/Http/Controllers/GoogleSheetController.php
+++ app/Http/Controllers/GoogleSheetController.php
@@ -3211,7 +3211,7 @@ class GoogleSheetController extends Controller
             return view('database.partials.senior_table', compact('data'))->render();
         }
 
-        return view('database.trainercompleted', compact('data'));
+        return view('database.trainercomplete', compact('data'));
     }
 
 
