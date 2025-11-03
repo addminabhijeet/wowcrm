2594c2f92780a97ebeae3b9c44016a36b234f0f5
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-03 17:54:29 +0530

    m

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -93,6 +93,27 @@ $script= '<script src="' . asset('assets/js/homeOneChart.js') . '"></script>';
             </div>
         </div><!-- card end -->
     </div>
+        <div class="col">
+        <div class="card shadow-none border bg-gradient-start-4 h-100">
+            <div class="card-body p-20">
+                <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
+                    <div>
+                        <p class="fw-medium text-primary-light mb-1">Total Trainer</p>
+                        <h6 class="mb-0">{{ $users->where('role', 'trainer')->count() }}</h6>
+                    </div>
+                    <div class="w-50-px h-50-px bg-success-main rounded-circle d-flex justify-content-center align-items-center">
+                        <iconify-icon icon="solar:wallet-bold" class="text-white text-2xl mb-0"></iconify-icon>
+                    </div>
+                </div>
+                <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
+                    <span class="d-inline-flex align-items-center gap-1 text-success-main">
+                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> 
+                    </span>
+                    Last 30 days Candidates
+                </p>
+            </div>
+        </div><!-- card end -->
+    </div>
 </div>
 
 <div class="row gy-4 mt-1">
