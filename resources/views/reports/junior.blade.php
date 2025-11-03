12b36773a4540d8f236085658b0382af2b5f86d8
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-03 22:05:30 +0530

    m

--- resources/views/reports/junior.blade.php
+++ resources/views/reports/junior.blade.php
@@ -533,7 +533,7 @@ $script = '<script>
     <div class="col-xxl-4 col-lg-6">
         <div class="card h-100 radius-8 border-0">
             <div class="card-body p-24">
-                <h6 class="mb-2 fw-bold text-lg">Statistic</h6>
+                <h6 class="mb-2 fw-bold text-lg">Total Summary (From Joining)</h6>
 
                 <div class="mt-24">
                     <div class="d-flex align-items-center gap-1 justify-content-between mb-44">
