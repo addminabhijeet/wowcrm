6c89568c9f4f71f6302951c2d38c796e5eaf66d4
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-06 05:13:28 +0530

    m

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -23,7 +23,7 @@ $script= '<script src="' . asset('assets/js/homeOneChart.js') . '"></script>';
                 </div>
                 <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
                     <span class="d-inline-flex align-items-center gap-1 text-success-main">
-                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> 
+                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon>
                     </span>
                     Last 30 days total users
                 </p>
@@ -44,9 +44,9 @@ $script= '<script src="' . asset('assets/js/homeOneChart.js') . '"></script>';
                 </div>
                 <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
                     <span class="d-inline-flex align-items-center gap-1 text-danger-main">
-                        <iconify-icon icon="bxs:down-arrow" class="text-xs"></iconify-icon> 
+                        <iconify-icon icon="bxs:down-arrow" class="text-xs"></iconify-icon>
                     </span>
-                    Last 30 days IT Recruiter Users
+                    Last 30 days IT Recruiter
                 </p>
             </div>
         </div><!-- card end -->
@@ -65,7 +65,7 @@ $script= '<script src="' . asset('assets/js/homeOneChart.js') . '"></script>';
                 </div>
                 <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
                     <span class="d-inline-flex align-items-center gap-1 text-success-main">
-                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> 
+                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon>
                     </span>
                     Last 30 days IT Senior Recruiter
                 </p>
@@ -77,7 +77,7 @@ $script= '<script src="' . asset('assets/js/homeOneChart.js') . '"></script>';
             <div class="card-body p-20">
                 <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
                     <div>
-                        <p class="fw-medium text-primary-light mb-1">Total Accounts</p>
+                        <p class="fw-medium text-primary-light mb-1">Total Support</p>
                         <h6 class="mb-0">{{ $users->where('role', 'accountant')->count() }}</h6>
                     </div>
                     <div class="w-50-px h-50-px bg-success-main rounded-circle d-flex justify-content-center align-items-center">
@@ -86,14 +86,14 @@ $script= '<script src="' . asset('assets/js/homeOneChart.js') . '"></script>';
                 </div>
                 <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
                     <span class="d-inline-flex align-items-center gap-1 text-success-main">
-                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> 
+                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon>
                     </span>
                     Last 30 days Candidates
                 </p>
             </div>
         </div><!-- card end -->
     </div>
-        <div class="col">
+    <div class="col">
         <div class="card shadow-none border bg-gradient-start-4 h-100">
             <div class="card-body p-20">
                 <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
@@ -107,7 +107,28 @@ $script= '<script src="' . asset('assets/js/homeOneChart.js') . '"></script>';
                 </div>
                 <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
                     <span class="d-inline-flex align-items-center gap-1 text-success-main">
-                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon> 
+                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon>
+                    </span>
+                    Last 30 days Candidates
+                </p>
+            </div>
+        </div><!-- card end -->
+    </div>
+        <div class="col">
+        <div class="card shadow-none border bg-gradient-start-4 h-100">
+            <div class="card-body p-20">
+                <div class="d-flex flex-wrap align-items-center justify-content-between gap-3">
+                    <div>
+                        <p class="fw-medium text-primary-light mb-1">Total Admin</p>
+                        <h6 class="mb-0">{{ $users->where('role', 'admin')->count() }}</h6>
+                    </div>
+                    <div class="w-50-px h-50-px bg-success-main rounded-circle d-flex justify-content-center align-items-center">
+                        <iconify-icon icon="solar:wallet-bold" class="text-white text-2xl mb-0"></iconify-icon>
+                    </div>
+                </div>
+                <p class="fw-medium text-sm text-primary-light mt-12 mb-0 d-flex align-items-center gap-2">
+                    <span class="d-inline-flex align-items-center gap-1 text-success-main">
+                        <iconify-icon icon="bxs:up-arrow" class="text-xs"></iconify-icon>
                     </span>
                     Last 30 days Candidates
                 </p>
