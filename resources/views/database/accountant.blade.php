edf1ef7020a636c6f62668e8845cd9795c1ae220
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-05 22:52:31 +0530

    m

--- resources/views/database/accountant.blade.php
+++ resources/views/database/accountant.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.layout')
 @php
 $title='Users Grid';
-$subTitle = 'Database j';
+$subTitle = 'Database';
 $script ='<script>
     $(".remove-item-btn").on("click", function() {
         $(this).closest("tr").addClass("d-none")
