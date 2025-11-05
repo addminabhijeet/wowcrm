a0c2fd80b2afe98af0d429619462bb24311981e7
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-05 21:30:09 +0530

    m

--- resources/views/candidate/accountant.php
+++ resources/views/candidate/accountant.php
@@ -17,15 +17,6 @@ $script = '<script>
             <span class="text-md fw-medium text-secondary-light mb-0">Show</span>
             <select class="form-select form-select-sm w-auto ps-12 py-6 radius-12 h-40-px">
                 <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-                <option>6</option>
-                <option>7</option>
-                <option>8</option>
-                <option>9</option>
-                <option>10</option>
             </select>
             <form class="navbar-search">
                 <input type="text" class="bg-base h-40-px w-auto" name="search" placeholder="Search">
@@ -83,18 +74,6 @@ $script = '<script>
                     <a class="page-link text-secondary-light fw-semibold radius-8 border-0 d-flex align-items-center justify-content-center h-32-px w-32-px text-md bg-primary-600 text-white" href="javascript:void(0)">1</a>
                 </li>
                 <li class="page-item">
-                    <a class="page-link bg-neutral-200 text-secondary-light fw-semibold radius-8 border-0 d-flex align-items-center justify-content-center h-32-px w-32-px" href="javascript:void(0)">2</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link bg-neutral-200 text-secondary-light fw-semibold radius-8 border-0 d-flex align-items-center justify-content-center h-32-px w-32-px text-md" href="javascript:void(0)">3</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link bg-neutral-200 text-secondary-light fw-semibold radius-8 border-0 d-flex align-items-center justify-content-center h-32-px w-32-px text-md" href="javascript:void(0)">4</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link bg-neutral-200 text-secondary-light fw-semibold radius-8 border-0 d-flex align-items-center justify-content-center h-32-px w-32-px text-md" href="javascript:void(0)">5</a>
-                </li>
-                <li class="page-item">
                     <a class="page-link bg-neutral-200 text-secondary-light fw-semibold radius-8 border-0 d-flex align-items-center justify-content-center h-32-px w-32-px text-md" href="javascript:void(0)">
                         <iconify-icon icon="ep:d-arrow-right" class=""></iconify-icon>
                     </a>
@@ -104,59 +83,7 @@ $script = '<script>
     </div>
 </div>
 
-<!-- Modal Start -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog modal-dialog-centered">
-        <div class="modal-content radius-16 bg-base">
-            <div class="modal-header py-16 px-24 border border-top-0 border-start-0 border-end-0">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Add New Role</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body p-24">
-                <form action="#">
-                    <div class="row">
-                        <div class="col-12 mb-20">
-                            <label class="form-label fw-semibold text-primary-light text-sm mb-8">Role Name</label>
-                            <input type="text" class="form-control radius-8" placeholder="Enter Role  Name">
-                        </div>
-                        <div class="col-12 mb-20">
-                            <label for="desc" class="form-label fw-semibold text-primary-light text-sm mb-8">Description</label>
-                            <textarea class="form-control" id="desc" rows="4" cols="50" placeholder="Write some text"></textarea>
-                        </div>
 
-                        <div class="col-12 mb-20">
-                            <label class="form-label fw-semibold text-primary-light text-sm mb-8">Status </label>
-                            <div class="d-flex align-items-center flex-wrap gap-28">
-                                <div class="form-check checked-success d-flex align-items-center gap-2">
-                                    <input class="form-check-input" type="radio" name="label" id="Personal">
-                                    <label class="form-check-label line-height-1 fw-medium text-secondary-light text-sm d-flex align-items-center gap-1" for="Personal">
-                                        <span class="w-8-px h-8-px bg-success-600 rounded-circle"></span>
-                                        Active
-                                    </label>
-                                </div>
-                                <div class="form-check checked-danger d-flex align-items-center gap-2">
-                                    <input class="form-check-input" type="radio" name="label" id="Holiday">
-                                    <label class="form-check-label line-height-1 fw-medium text-secondary-light text-sm d-flex align-items-center gap-1" for="Holiday">
-                                        <span class="w-8-px h-8-px bg-danger-600 rounded-circle"></span>
-                                        Inactive
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-center gap-3 mt-24">
-                            <button type="reset" class="border border-danger-600 bg-hover-danger-200 text-danger-600 text-md px-40 py-11 radius-8">
-                                Cancel
-                            </button>
-                            <button type="submit" class="btn btn-primary border border-primary-600 text-md px-48 py-12 radius-8">
-                                Save
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 <!-- Modal End -->
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 @endsection
