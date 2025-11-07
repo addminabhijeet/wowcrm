b812b448964dd9eda37d9da7a649236cdb588e58
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-08 04:18:20 +0530

    m

--- resources/views/database/admin.blade.php
+++ resources/views/database/admin.blade.php
@@ -31,14 +31,13 @@ $script ='<script>
                 @foreach ($juniorUsers as $junior)
                 <option value="{{ $junior->id }}">
                     {{ $junior->name }}
-                    @if({{ {{ $junior->designation === 'senior' ? 'IT Senior Recruiter' : ($junior->designation === 'junior' ? 'IT Recruiter' : $junior->designation) }}
-                    }}
-                    ) ({{ {{ {{ $junior->designation === 'senior' ? 'IT Senior Recruiter' : ($junior->designation === 'junior' ? 'IT Recruiter' : $junior->designation) }}
-                    }}
-                    }}) @endif
+                    @if($junior->designation)
+                    ({{ $junior->designation === 'senior' ? 'IT Senior Recruiter' : ($junior->designation === 'junior' ? 'IT Recruiter' : $junior->designation) }})
+                    @endif
                 </option>
                 @endforeach
             </select>
+
 
 
         </div>
