34213e66768c02a0a8730c2e5e62eab2cf1ecef5
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-14 02:44:49 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -178,7 +178,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/dashboard/junior/google-sheet', [GoogleSheetController::class, 'junior'])->name('google.sheet.junior');
     Route::get('/dashboard/junior/google-sheet-candm', [GoogleSheetController::class, 'juniorcandm'])->name('google.sheet.junior.candm');
-    Route::get('/dashboard/junior/google-sheet-modcandm', [GoogleSheetController::class, 'juniormodcandm'])->name('google.sheet.junior.modcandm');
+    Route::get('/dashboard/junior/google-sheet-modcandm', [GoogleSheetController::class, 'juniormodcandm'])->name('google.sheet.juniormodcandm');
     Route::post('/dashboard/junior/google-sheet-candm-update', [GoogleSheetController::class, 'juniorcandmupdate'])->name('juniorcandmupdate');
     Route::post('/dashboard/senior/google-sheet-candm-update', [GoogleSheetController::class, 'seniorcandmupdate'])->name('seniorcandmupdate');
     Route::post('/dashboard/junior/google-sheet/fetch', [GoogleSheetController::class, 'juniorfetch'])->name('google.sheet.juniorfetch');
