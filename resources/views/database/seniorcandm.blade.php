81078ca3df28dda8af6e8beb6cdc2b6ba63bcc04
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-18 04:07:44 +0530

    m

--- resources/views/database/seniorcandm.blade.php
+++ resources/views/database/seniorcandm.blade.php
@@ -891,7 +891,7 @@ $script ='<script>
         // -----------------------------
         function fetchTable(search = '', page = 1, junior_user = '', row_id = '') {
             $.ajax({
-                url: "{{ route('google.sheet.senior') }}",
+                url: "{{ route('google.sheet.seniorcandm') }}",
                 type: 'GET',
                 data: {
                     search,
@@ -1229,7 +1229,7 @@ $script ='<script>
         let juniorId = this.value;
         let search = document.getElementById('senior-search').value;
 
-        fetch("{{ route('google.sheet.senior') }}?junior_user=" + juniorId + "&search=" + search, {
+        fetch("{{ route('google.sheet.seniorcandm') }}?junior_user=" + juniorId + "&search=" + search, {
                 headers: {
                     'X-Requested-With': 'XMLHttpRequest'
                 }
