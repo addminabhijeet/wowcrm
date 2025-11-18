36c6db20d077d580844a8acb2c6cebea3a085e8c
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-18 16:13:29 +0530

    m

--- resources/views/database/career.blade.php
+++ resources/views/database/career.blade.php
@@ -241,12 +241,6 @@ $script ='<script>
                             <a href="#" download class="btn btn-sm btn-secondary download-btn d-none">Download</a>
                             @endif
                         </td>
-
-                        <td class="text-center">
-                            <button class="btn btn-sm btn-success save-btn" data-id="{{ $row->id }}">
-                                <i class="fas fa-save"></i> Save
-                            </button>
-                        </td>
                     </tr>
                     @endforeach
                 </tbody>
