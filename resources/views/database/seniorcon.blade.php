95681aca4d9bf2aec349564385f633dff48e5207
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-19 22:31:09 +0530

    m

--- resources/views/database/seniorcon.blade.php
+++ resources/views/database/seniorcon.blade.php
@@ -175,7 +175,7 @@ $script ='<script>
 
                         {{-- Exe Remarks --}}
                         <td>
-                            @php $exeOptions = ['Called & Mailed','Not Interested','Not Connected','Did Not Pickup','Others','Ready To Paid','VM','Busy']; @endphp
+                            @php $exeOptions = ['Verification Completed','Payment Completed']; @endphp
                             <select class="form-select dynamic-dropdown" data-key="Exe Remarks">
                                 <option value="">-- Exe Remarks --</option>
                                 @foreach($exeOptions as $option)
