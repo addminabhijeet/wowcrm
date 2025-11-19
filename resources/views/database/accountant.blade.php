e754990f654dd84d68191f9131a133c16a537e7f
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-19 18:08:43 +0530

    m

--- resources/views/database/accountant.blade.php
+++ resources/views/database/accountant.blade.php
@@ -752,21 +752,21 @@ $script ='<script>
     ">
 
         <!-- SINGLE IFRAME CARD -->
-        <div style="background:#fff; padding:18px; border-radius:12px; max-width: 820px;   
+        <div style="background:#fff; padding:18px; border-radius:12px; max-width: 520px;   
             box-shadow:0 4px 18px rgba(0,0,0,0.08); border:1px solid #eee;">
             <h4 style="margin:0 0 12px; font-size:17px; color:#444;">Acceptance Form</h4>
             <iframe 
                 src="{{ route('pdf.acceptance') }}?${queryParams}"
-                style="width:700px height:1200px; border:0; border-radius:10px;">
+                style="width:100%; height:1200px; border:0; border-radius:10px;">
             </iframe>
         </div>
 
-        <div style="background:#fff; padding:18px; border-radius:12px; max-width: 820px;   
+        <div style="background:#fff; padding:18px; border-radius:12px; max-width: 620px;   
             box-shadow:0 4px 18px rgba(0,0,0,0.08); border:1px solid #eee;">
             <h4 style="margin:0 0 12px; font-size:17px; color:#444;">Consultation Form</h4>
             <iframe 
                 src="{{ route('pdf.consultation') }}?${queryParams}"
-                style="width:500px height:1200px; border:0; border-radius:10px;">
+                style="width:100%; height:1200px; border:0; border-radius:10px;">
             </iframe>
         </div>
 
