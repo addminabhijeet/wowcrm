fd0ccaedbec214d9f292be211152f668641be882
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-19 19:12:18 +0530

    m

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -8,103 +8,152 @@ class PdfController extends Controller
 {
     public function acceptance(Request $request)
     {
-        $name   = $request->input('name');
-        $email  = $request->input('email');
-        $amount = $request->input('amount');
-        $course = $request->input('course');
-        $remark = $request->input('remark');
+        $name          = $request->input('name');
+        $email         = $request->input('email');
+        $amount        = $request->input('amount');
+        $course        = $request->input('course');
+        $remark        = $request->input('remark');
+        $tranId        = $request->input('tranId');
+        $tranRef       = $request->input('tranRef');
+        $paymentMethod = $request->input('paymentMethod');
+        $payeeName     = $request->input('payeeName');
 
         return view('pdf.acceptance', compact(
             'name',
             'email',
             'amount',
             'course',
-            'remark'
+            'remark',
+            'tranId',
+            'tranRef',
+            'paymentMethod',
+            'payeeName'
         ));
     }
 
+
     public function consultation(Request $request)
     {
-        $name   = $request->input('name');
-        $email  = $request->input('email');
-        $amount = $request->input('amount');
-        $course = $request->input('course');
-        $remark = $request->input('remark');
+        $name          = $request->input('name');
+        $email         = $request->input('email');
+        $amount        = $request->input('amount');
+        $course        = $request->input('course');
+        $remark        = $request->input('remark');
+        $tranId        = $request->input('tranId');
+        $tranRef       = $request->input('tranRef');
+        $paymentMethod = $request->input('paymentMethod');
+        $payeeName     = $request->input('payeeName');
 
         return view('pdf.consultation', compact(
             'name',
             'email',
             'amount',
             'course',
-            'remark'
+            'remark',
+            'tranId',
+            'tranRef',
+            'paymentMethod',
+            'payeeName'
         ));
     }
 
     public function delivery(Request $request)
     {
-        $name   = $request->input('name');
-        $email  = $request->input('email');
-        $amount = $request->input('amount');
-        $course = $request->input('course');
-        $remark = $request->input('remark');
+        $name          = $request->input('name');
+        $email         = $request->input('email');
+        $amount        = $request->input('amount');
+        $course        = $request->input('course');
+        $remark        = $request->input('remark');
+        $tranId        = $request->input('tranId');
+        $tranRef       = $request->input('tranRef');
+        $paymentMethod = $request->input('paymentMethod');
+        $payeeName     = $request->input('payeeName');
 
         return view('pdf.delivery', compact(
             'name',
             'email',
             'amount',
             'course',
-            'remark'
+            'remark',
+            'tranId',
+            'tranRef',
+            'paymentMethod',
+            'payeeName'
         ));
     }
 
     public function payment(Request $request)
     {
-        $name   = $request->input('name');
-        $email  = $request->input('email');
-        $amount = $request->input('amount');
-        $course = $request->input('course');
-        $remark = $request->input('remark');
+        $name          = $request->input('name');
+        $email         = $request->input('email');
+        $amount        = $request->input('amount');
+        $course        = $request->input('course');
+        $remark        = $request->input('remark');
+        $tranId        = $request->input('tranId');
+        $tranRef       = $request->input('tranRef');
+        $paymentMethod = $request->input('paymentMethod');
+        $payeeName     = $request->input('payeeName');
 
         return view('pdf.payment', compact(
             'name',
             'email',
             'amount',
             'course',
-            'remark'
+            'remark',
+            'tranId',
+            'tranRef',
+            'paymentMethod',
+            'payeeName'
         ));
     }
 
     public function deliveryuk(Request $request)
     {
-        $name   = $request->input('name');
-        $email  = $request->input('email');
-        $amount = $request->input('amount');
-        $course = $request->input('course');
-        $remark = $request->input('remark');
+        $name          = $request->input('name');
+        $email         = $request->input('email');
+        $amount        = $request->input('amount');
+        $course        = $request->input('course');
+        $remark        = $request->input('remark');
+        $tranId        = $request->input('tranId');
+        $tranRef       = $request->input('tranRef');
+        $paymentMethod = $request->input('paymentMethod');
+        $payeeName     = $request->input('payeeName');
 
         return view('pdf.deliveryuk', compact(
             'name',
             'email',
             'amount',
             'course',
-            'remark'
+            'remark',
+            'tranId',
+            'tranRef',
+            'paymentMethod',
+            'payeeName'
         ));
     }
 
     public function paymentuk(Request $request)
     {
-        $name   = $request->input('name');
-        $email  = $request->input('email');
-        $amount = $request->input('amount');
-        $course = $request->input('course');
-        $remark = $request->input('remark');
+        $name          = $request->input('name');
+        $email         = $request->input('email');
+        $amount        = $request->input('amount');
+        $course        = $request->input('course');
+        $remark        = $request->input('remark');
+        $tranId        = $request->input('tranId');
+        $tranRef       = $request->input('tranRef');
+        $paymentMethod = $request->input('paymentMethod');
+        $payeeName     = $request->input('payeeName');
 
         return view('pdf.paymentuk', compact(
             'name',
             'email',
             'amount',
             'course',
-            'remark'
+            'remark',
+            'tranId',
+            'tranRef',
+            'paymentMethod',
+            'payeeName'
         ));
     }
 }
