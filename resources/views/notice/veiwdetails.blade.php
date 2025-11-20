22ffd3876f197f74eba9bbcd6b0a5a8649f1d46d
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-21 02:42:12 +0530

    m

--- resources/views/notice/veiwdetails.blade.php
+++ resources/views/notice/veiwdetails.blade.php
@@ -23,125 +23,6 @@ $script ='<script>
 @section('content')
 
 <div class="row gy-4">
-    <div class="col-xxl-3">
-        <div class="card h-100 p-0">
-            <div class="card-body p-24">
-
-                <div class="mt-16">
-                    <ul>
-                        <li class="item-active mb-4">
-                            <a href="" class="bg-hover-primary-50 px-12 py-8 w-100 radius-8 text-secondary-light">
-                                <span class="d-flex align-items-center gap-10 justify-content-between w-100">
-                                    <span class="d-flex align-items-center gap-10">
-                                        <span class="icon text-xxl line-height-1 d-flex">
-                                            <iconify-icon icon="uil:envelope" class="icon line-height-1"></iconify-icon>
-                                        </span>
-                                        <span class="fw-semibold">Inbox</span>
-                                    </span>
-                                    <span class="fw-medium">800</span>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="" class="bg-hover-primary-50 px-12 py-8 w-100 radius-8 text-secondary-light">
-                                <span class="d-flex align-items-center gap-10 justify-content-between w-100">
-                                    <span class="d-flex align-items-center gap-10">
-                                        <span class="icon text-xxl line-height-1 d-flex">
-                                            <iconify-icon icon="ph:star-bold" class="icon line-height-1"></iconify-icon>
-                                        </span>
-                                        <span class="fw-semibold">Starred</span>
-                                    </span>
-                                    <span class="fw-medium">250</span>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="" class="bg-hover-primary-50 px-12 py-8 w-100 radius-8 text-secondary-light">
-                                <span class="d-flex align-items-center gap-10 justify-content-between w-100">
-                                    <span class="d-flex align-items-center gap-10">
-                                        <span class="icon text-xxl line-height-1 d-flex">
-                                            <iconify-icon icon="ion:paper-plane-outline" class="icon line-height-1"></iconify-icon>
-                                        </span>
-                                        <span class="fw-semibold">Sent</span>
-                                    </span>
-                                    <span class="fw-medium">80</span>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="" class="bg-hover-primary-50 px-12 py-8 w-100 radius-8 text-secondary-light">
-                                <span class="d-flex align-items-center gap-10 justify-content-between w-100">
-                                    <span class="d-flex align-items-center gap-10">
-                                        <span class="icon text-xxl line-height-1 d-flex">
-                                            <iconify-icon icon="lucide:pencil" class="icon line-height-1"></iconify-icon>
-                                        </span>
-                                        <span class="fw-semibold">Draft</span>
-                                    </span>
-                                    <span class="fw-medium">50</span>
-                                </span>
-                            </a>
-                        </li>
-                        <li class="mb-4">
-                            <a href="" class="bg-hover-primary-50 px-12 py-8 w-100 radius-8 text-secondary-light">
-                                <span class="d-flex align-items-center gap-10 justify-content-between w-100">
-                                    <span class="d-flex align-items-center gap-10">
-                                        <span class="icon text-xxl line-height-1 d-flex">
-                                            <iconify-icon icon="ph:warning-bold" class="icon line-height-1"></iconify-icon>
-                                        </span>
-                                        <span class="fw-semibold">Spam</span>
-                                    </span>
-                                    <span class="fw-medium">30</span>
-                                </span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" class="bg-hover-primary-50 px-12 py-8 w-100 radius-8 text-secondary-light">
-                                <span class="d-flex align-items-center gap-10 justify-content-between w-100">
-                                    <span class="d-flex align-items-center gap-10">
-                                        <span class="icon text-xxl line-height-1 d-flex">
-                                            <iconify-icon icon="material-symbols:delete-outline" class="icon line-height-1"></iconify-icon>
-                                        </span>
-                                        <span class="fw-semibold">Bin</span>
-                                    </span>
-                                    <span class="fw-medium">20</span>
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                    <div class="mt-24">
-                        <h6 class="text-lg fw-semibold text-primary-light mb-16">TAGS</h6>
-                        <ul>
-                            <li class="mb-20">
-                                <span class="line-height-1 fw-medium text-secondary-light text-sm d-flex align-items-center gap-10">
-                                    <span class="w-8-px h-8-px bg-primary-600 rounded-circle"></span>
-                                    Personal
-                                </span>
-                            </li>
-                            <li class="mb-20">
-                                <span class="line-height-1 fw-medium text-secondary-light text-sm d-flex align-items-center gap-10">
-                                    <span class="w-8-px h-8-px bg-lilac-600 rounded-circle"></span>
-                                    Social
-                                </span>
-                            </li>
-                            <li class="mb-20">
-                                <span class="line-height-1 fw-medium text-secondary-light text-sm d-flex align-items-center gap-10">
-                                    <span class="w-8-px h-8-px bg-success-600 rounded-circle"></span>
-                                    Promotions
-                                </span>
-                            </li>
-                            <li class="mb-20">
-                                <span class="line-height-1 fw-medium text-secondary-light text-sm d-flex align-items-center gap-10">
-                                    <span class="w-8-px h-8-px bg-warning-600 rounded-circle"></span>
-                                    Business
-                                </span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
     <div class="col-xxl-9">
         <div class="card h-100 p-0 email-card overflow-x-auto d-block">
             <div class="min-w-450-px d-flex flex-column justify-content-between h-100">
@@ -150,8 +31,7 @@ $script ='<script>
                         <button class="text-secondary-light d-flex me-8">
                             <iconify-icon icon="mingcute:arrow-left-line" class="icon fs-3 line-height-1"></iconify-icon>
                         </button>
-                        <h6 class="mb-0 text-lg">Kathryn Murphy</h6>
-                        <span class="bg-primary-50 text-primary-600 text-sm radius-4 px-8">Personal</span>
+                        <h6 class="mb-0 text-lg">Payment</h6>
                     </div>
                     <div class="d-flex align-items-center gap-3">
                         <button class="text-secondary-light d-flex">
