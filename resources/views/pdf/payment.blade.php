7834c6769f0656a51001eea74d319e025b0e88d3
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-24 23:58:58 +0530

    m

--- resources/views/pdf/payment.blade.php
+++ resources/views/pdf/payment.blade.php
@@ -122,7 +122,11 @@ src: url(data:application/font-woff;charset=utf-8;base64,d09GRgABAAAAAB/4AA0AAAA
 <span class="t s2" style="left:149px;bottom:805px;letter-spacing:0.17px;">Status: </span><span class="t s7" style="left:195px;bottom:805px;letter-spacing:0.13px;">Paid </span>
 <span class="t s4" style="left:146px;bottom:636px;letter-spacing:-0.06px;">Description </span><span class="t s4" style="left:543px;bottom:636px;letter-spacing:-0.04px;">Quantity </span><span class="t s4" style="left:674px;bottom:636px;letter-spacing:-0.06px;">Price </span><span class="t s4" style="left:766px;bottom:636px;letter-spacing:-0.07px;">Amount </span>
 <span class="t s8" style="left:137px;bottom:1036px;letter-spacing:-0.04px;">Transaction </span><span class="t s8" style="left:220px;bottom:1036px;letter-spacing:-0.08px;">Reference </span><span class="t s8" style="left:292px;bottom:1036px;">#</span><span class="t s8" style="left:301px;bottom:1036px;">: </span><span class="t s7" style="left:309px;bottom:1037px;letter-spacing:0.23px;">{{$tranRef}} </span>
-<span class="t s9" style="left:701px;bottom:807px;letter-spacing:-0.06px;word-spacing:-0.07px;">{{$payeeName}} </span>
+<span class="t s9" 
+      style="left:701px; bottom:807px; letter-spacing:-0.06px; word-spacing:-0.07px; display:inline-block; min-width:250px; text-align:right;">
+    {{$payeeName}}
+</span>
+
 <span class="t sa" style="left:674px;bottom:779px;letter-spacing:-0.06px;">{{$email}} </span>
 <span class="t s2" style="left:707px;bottom:753px;letter-spacing:0.23px;">+1 </span><span class="t sa" style="left:728px;bottom:752px;letter-spacing:-0.05px;word-spacing:-0.17px;">{{$phone_Number}} </span>
 <span class="t s2" style="left:552px; bottom:727px; letter-spacing:-0.1px; word-spacing:-0.07px; display:inline-block; min-width:250px; text-align:right;">{{$Location}}</span>
