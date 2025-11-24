48c7ad8890ddf87b6ad3bd83ba709f692de6440c
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-24 22:52:14 +0530

    m

--- resources/views/pdf/delivery.blade.php
+++ resources/views/pdf/delivery.blade.php
@@ -112,7 +112,10 @@ src: url(data:application/font-woff;charset=utf-8;base64,d09GRgABAAAAAE2MAA0AAAA
 <span class="t s9" style="left:735px;bottom:952px;letter-spacing:-0.11px;word-spacing:0.04px;">{{$payeeName}} </span>
 <span class="t sa" style="left:688px;bottom:931px;letter-spacing:-0.12px;">{{$email}} </span>
 <span class="t sa" style="left:725px;bottom:910px;letter-spacing:-0.06px;word-spacing:-0.16px;">{{$phone_Number}} </span>
-<span class="t sa" style="left:470px;bottom:889px;letter-spacing:-0.1px;word-spacing:-0.07px;">{{$Location}} </span></div>
+<span class="t sa" style="left:470px; bottom:889px; letter-spacing:-0.1px; word-spacing:-0.07px; display: block; text-align: right;">
+    {{$Location}}
+</span>
+
 
 </div>
 
