cc0287aa0a5809b05c856daeaaed900976842c74
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-24 18:31:13 +0530

    m

--- resources/views/pdf/acceptance.blade.php
+++ resources/views/pdf/acceptance.blade.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <!-- Required Libraries -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
     <style>
         body {
             background-color: #444;
