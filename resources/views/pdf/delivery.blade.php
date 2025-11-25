624b137df001ee64699fb620b4231886a0e7ec39
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-25 17:31:05 +0530

    m

--- resources/views/pdf/delivery.blade.php
+++ resources/views/pdf/delivery.blade.php
@@ -122,10 +122,10 @@ src: url(data:application/font-woff;charset=utf-8;base64,d09GRgABAAAAAE2MAA0AAAA
 <span class="t s2" style="left:135px;bottom:663px;">1 </span><span class="t s2" style="left:277px;bottom:663px;letter-spacing:-0.08px;word-spacing:-0.11px;">Job Workshop and Consulting Fees </span><span class="t s2" style="left:624px;bottom:663px;letter-spacing:-0.08px;">{{$amount}} </span><span class="t s2" style="left:753px;bottom:663px;letter-spacing:-0.08px;">{{$amount}} </span>
 <span class="t s1" style="left:620px;bottom:567px;letter-spacing:0.04px;">TOTAL </span><span class="t s3" style="left:753px;bottom:568px;letter-spacing:-0.04px;">{{$amount}} </span>
 <span class="t s4" style="left:130px;bottom:435px;letter-spacing:0.17px;word-spacing:-0.03px;">Customer/Purchaser/Buyer's Declaration: </span>
-<span class="t s5" style="left:130px;bottom:403px;letter-spacing:-0.31px;">I hereby certify that the seller, Norlox Solutions LLC, provided the aforementioned goods and services to me. </span>
+<span class="t s5" style="left:130px;bottom:403px;letter-spacing:-0.31px;">I hereby certify that the seller, Norlox Solutions Pvt. Ltd., provided the aforementioned goods and services to me. </span>
 <span class="t v0 s6" style="left:130px;bottom:245px;letter-spacing:0.03px;">Buyer’s </span><span class="t s6" style="left:173px;bottom:245px;letter-spacing:0.18px;">Signature: </span><span class="t s6" style="left:600px;bottom:245px;letter-spacing:0.21px;">Date: </span><span class="t s6" style="left:667px;bottom:245px;letter-spacing:0.17px;">________________________ </span>
 <span class="t s7" style="left:106px;bottom:1094px;letter-spacing:-0.21px;word-spacing:-0.02px;">DELIVERY RECEIPT </span>
-<span class="t s8" style="left:628px;bottom:1059px;letter-spacing:0.04px;word-spacing:0.1px;">NORLOX SOLUTIONS LLC </span>
+<span class="t s8" style="left:628px;bottom:1059px;letter-spacing:0.04px;word-spacing:0.1px;">Norlox Solutions Pvt. Ltd. </span>
 <span class="t s6" style="left:109px;bottom:992px;letter-spacing:0.15px;word-spacing:-0.05px;">{{$paymentDate}} </span>
 <span class="t s6" style="left:675px;bottom:985px;letter-spacing:0.14px;word-spacing:-0.05px;">Services/goods delivered to: </span>
 <span class="t s9" style="left:735px;bottom:952px;letter-spacing:-0.11px;word-spacing:0.04px;">{{$payeeName}} </span>
