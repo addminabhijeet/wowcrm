eac2ce8c8bee96004f7e3cf4dbc2c3da9590604b
addminabhijeet <addmin.abhijeet@gmail.com>
2025-11-27 00:15:56 +0530

    m

--- resources/views/database/accountantver.blade.php
+++ resources/views/database/accountantver.blade.php
@@ -68,6 +68,7 @@
                                 <th scope="col">Reference Number</th>
                                 <th scope="col">Payment Method</th>
                                 <th scope="col">Payee Name</th>
+                                <th scope="col">Payee Name</th>
                                 <th scope="col">Acceptance</th>
                                 <th scope="col">Consultation</th>
                                 <th scope="col">Delivery</th>
