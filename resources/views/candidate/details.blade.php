790c64b0e6b0f34e1c27ccd6505e9447cd091c8b
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-02 02:20:46 +0530

    m

--- resources/views/candidate/details.blade.php
+++ resources/views/candidate/details.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.layout')
 @php
-    $title = $candidate->Name;
+    $title = $candidate->Name;($candidate->sheet_row_number;)
     $subTitle = 'Senior Support Associate';
     $script = '<script>
         // ======================== Upload Image Start =====================
