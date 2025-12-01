ff36137f51abe2ad9cbee7df94748ed885e8b792
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-01 23:47:15 +0530

    m

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,13 +5,16 @@
     <!-- Standard meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!-- CSRF Token for AJAX -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    
+
     <!-- Page title -->
     <title>{{ isset($title) ? $title : config('app.name') }}</title>
-    
+
+     <!-- Favicon -->
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+
     <!-- Styles and other head includes -->
     <x-head />
 </head>
@@ -29,9 +32,9 @@
         <div class="dashboard-main-body">
 
             <!-- Breadcrumb -->
-            <x-breadcrumb 
-                title="{{ isset($title) ? $title : '' }}" 
-                subTitle="{{ isset($subTitle) ? $subTitle : '' }}" 
+            <x-breadcrumb
+                title="{{ isset($title) ? $title : '' }}"
+                subTitle="{{ isset($subTitle) ? $subTitle : '' }}"
             />
 
             <!-- Main content -->
