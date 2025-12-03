bb7fd78d4f7ee5a1e997139ad32b40a1385ade59
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-03 17:49:26 +0530

    m

--- resources/views/reports/seniormonthly.blade.php
+++ resources/views/reports/seniormonthly.blade.php
@@ -557,7 +557,7 @@
                             <h5 class="fw-bold mb-1">{{ $juniorUser->name }}</h5>
                         </div>
                         <form method="GET"
-                            action="{{ route('call.reports.allseniormonthly') }}"
+                            action="{{ route('call.reports.seniormonthly') }}"
                             class="d-flex align-items-center gap-2">
                             <label for="selected_month" class="form-label mb-0 fw-semibold small">Select
                                 Month:</label>
