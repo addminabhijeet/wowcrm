209f1163fd92329235f37128adab1ec4907c3c2c
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-04 04:51:30 +0530

    m

--- resources/views/candidate/details.blade.php
+++ resources/views/candidate/details.blade.php
@@ -187,7 +187,7 @@
                                             </div>
 
                                             <div class="mb-20">
-                                                <label class="form-label fw-semibold text-primary-light text-sm mb-8">
+                                                <label class="form-label fw-semibold text-primary-light text-sm mb-8">-
                                                 </label>
                                                 <button type="button" id="save-profile-btn"
                                                     class="btn btn-success mb-10">
@@ -399,7 +399,7 @@
 
                                             <!-- Existing Save Button -->
                                             <div class="mb-20">
-                                                <label class="form-label fw-semibold text-primary-light text-sm mb-8">
+                                                <label class="form-label fw-semibold text-primary-light text-sm mb-8">-
                                                 </label>
                                                 <button type="button" id="save-edu-btn" class="btn btn-success mb-10">
                                                     Save
@@ -560,7 +560,7 @@
 
                                             <!--  Save Button -->
                                             <div class="mb-20">
-                                                <label class="form-label fw-semibold text-primary-light text-sm mb-8">
+                                                <label class="form-label fw-semibold text-primary-light text-sm mb-8">-
                                                 </label>
                                                 <button type="button" id="save-pay-btn" class="btn btn-success mb-10">
                                                     Save
