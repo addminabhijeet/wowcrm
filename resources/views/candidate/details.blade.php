84863762491da983a96dea18f7374b1adac40c07
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-04 00:10:07 +0530

    m

--- resources/views/candidate/details.blade.php
+++ resources/views/candidate/details.blade.php
@@ -427,8 +427,10 @@
                                         placeholder="Select a date to view remarks" readonly></textarea>
 
                                     <textarea id="new-remark" class="form-control radius-8 mb-2" rows="2" placeholder="Add new remark"></textarea>
-                                    <button type="button" id="add-remark-btn" class="btn btn-primary mb-10">Add
-                                        Remark</button>
+                                    <button type="button" id="save-btn" class="btn btn-success mb-10">
+                                        Save
+                                    </button>
+
 
                                     <!-- Hidden input to store all followups for form submission -->
                                     <textarea name="followups" id="followups" class="d-none">{{ old('followups', $candidate->followup ?? '') }}</textarea>
