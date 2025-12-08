4d3e41095d402c5611f4a648d2c33f6312f5576f
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-09 04:05:29 +0530

    m

--- resources/views/database/seniorcandm.blade.php
+++ resources/views/database/seniorcandm.blade.php
@@ -271,7 +271,7 @@
                                             value="{{ $row->Remark ?? '' }}" placeholder="Type remark">
                                     </td>
 
-                                    {{-- Exe Remarks --}}
+                                    {{-- Status --}}
                                     <td>
                                         @php
                                             $exeOptions = [
--- resources/views/database/seniormod.blade.php
+++ resources/views/database/seniormod.blade.php
@@ -289,7 +289,7 @@
                                             value="{{ $row->Remark ?? '' }}">
                                     </td>
 
-                                    {{-- Exe Remarks --}}
+                                    {{-- Status --}}
                                     <td>
                                         @php $exeOptions = ['Called & Mailed','Not Interested','Not Connected','Did Not Pickup','Others','Ready To Pay','VM','Busy']; @endphp
                                         <select class="form-select dynamic-dropdown" data-key="Exe Remarks">
