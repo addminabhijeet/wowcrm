bc40da85f55ec3fb4b4d821284c8c4cdc87b7340
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-08 17:41:51 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -109,7 +109,7 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/dashboard/admin/customer/update/{id}', [UserController::class, 'customerupdate'])->name('users.customer.update');
     Route::get('/dashboard/admin/customer/destroy/{id}', [UserController::class, 'customerdestroy'])->name('users.customer.destroy');
     Route::get('/dashboard/admin/latest-notification', [DashboardController::class, 'latestNotification'])->name('admin.latest.notification');
-    Route::get('/dashboard/admin/latest-markallread', [DashboardController::class, 'markAllRead'])->name('admin.notifications.markallread');
+    Route::put('/dashboard/admin/latest-markallread', [DashboardController::class, 'markAllRead'])->name('admin.notifications.markallread');
     Route::get('/dashboard/notification', [DashboardController::class, 'adminnotification'])->name('admin.notifications');
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.admin');
     Route::get('/dashboard/junior', [DashboardController::class, 'junior'])->name('dashboard.junior');
