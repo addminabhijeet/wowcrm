f835e91f1c6302c2dca9012ece6609de40b980aa
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-09 15:57:53 +0530

    m

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -360,8 +360,50 @@
                         <span>Database</span>
                     </a>
                     <ul class="sidebar-submenu">
-                        <li><a href="{{ route('google.sheet.index') }}"><i
-                                    class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Database</a></li>
+                        <li><a href="{{ route('google.sheet.seniormod') }}"><i
+                                    class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Junior Other
+                                Disposition</a>
+                        </li>
+                    </ul>
+                    <ul class="sidebar-submenu">
+                        <li><a href="{{ route('google.sheet.senior') }}"><i
+                                    class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Junior Called &
+                                Mailed</a></li>
+                    </ul>
+                    <ul class="sidebar-submenu">
+                        <li><a href="{{ route('google.sheet.seniorcandm') }}"><i
+                                    class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Senior Called &
+                                Mailed</a></li>
+                    </ul>
+                    <ul class="sidebar-submenu">
+                        <li><a href="{{ route('google.sheet.seniorpaid') }}"><i
+                                    class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Ready To Pay</a>
+                        </li>
+                    </ul>
+                    <ul class="sidebar-submenu">
+                        <li><a href="{{ route('google.sheet.seniorcon') }}"><i
+                                    class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Verification
+                                Call</a>
+                        </li>
+                    </ul>
+                    <ul class="sidebar-submenu">
+                        <li><a href="{{ route('google.sheet.accountant') }}"><i
+                                    class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Payment
+                                Completed</a></li>
+                    </ul>
+                    <ul class="sidebar-submenu">
+                        <li><a href="{{ route('google.sheet.accountantver') }}"><i
+                                    class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Status Pending</a>
+                        </li>
+                    </ul>
+                    <ul class="sidebar-submenu">
+                        <li><a href="{{ route('google.sheet.accountantpaid') }}"><i
+                                    class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>Status Completed</a>
+                        </li>
+                    </ul>
+                    <ul class="sidebar-submenu">
+                        <li><a href="{{ route('google.sheet.seniorassociate') }}"><i
+                                    class="ri-circle-fill circle-icon text-warning-main w-auto"></i>Support</a></li>
                     </ul>
                 </li>
 
