331cf56af6eacfd43fcbd4333d461ea1a62a5051
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-12 01:37:16 +0530

    m

--- app/Http/Controllers/GoogleSheetController.php
+++ app/Http/Controllers/GoogleSheetController.php
@@ -982,15 +982,14 @@ class GoogleSheetController extends Controller
 
         $query = GoogleSheetData::where(function ($q) use ($authUser, $userPattern) {
             $q->where('created_by', $authUser->id . '|junior')
-                ->whereRaw("RIGHT(created_by, LENGTH(?)) = ?", [$authUser->id . '|junior', $authUser->id . '|junior'])
+
                 ->orWhere('created_by', 'LIKE', $userPattern);
         })->where('transfers', 1);
 
         // Filter by selected junior
         if ($juniorUserId) {
             $query->where(function ($q) use ($juniorUserId) {
-                $q->where('created_by', 'LIKE', '%' . $juniorUserId . '|junior%')
-                    ->whereRaw("RIGHT(created_by, LENGTH(?)) = ?", [$juniorUserId . '|junior', $juniorUserId . '|junior']);
+                $q->where('created_by', 'LIKE', '%' . $juniorUserId . '|junior%');
             })->where('transfers', 1);
         }
 
