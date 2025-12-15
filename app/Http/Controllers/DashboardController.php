95f4e2223228fa246813116b97c5db99b1ffef9f
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-16 04:35:32 +0530

    m

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -901,10 +901,7 @@ class DashboardController extends Controller
 
             if ($secondsPassed > 0) {
                 // 🔥 FIX: decrement like a real clock
-                $timer->remaining_seconds = max(
-                    0,
-                    $timer->remaining_seconds + $secondsPassed
-                );
+                $timer->remaining_seconds = max(0, $timer->remaining_seconds + ($secondsPassed / 2));
             }
         }
 
