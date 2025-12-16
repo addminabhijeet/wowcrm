1380aea500c5f84c3791695c0a88f95cb44e105e
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-16 23:25:27 +0530

    m

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -892,13 +892,10 @@ class DashboardController extends Controller
 
         $workDaySeconds = $timerSetting->work_day_seconds;
 
+        // Update remaining seconds if timer is running
         if ($timer->status === 'running') {
             $secondsPassed = $currentTime->diffInSeconds($timer->updated_at);
-            if ($secondsPassed > 3) {
-                $timer->remaining_seconds = max(0, $timer->remaining_seconds - $secondsPassed);
-            } else {
-                $timer->remaining_seconds = max(0, $timer->remaining_seconds - 1);
-            }
+            $timer->remaining_seconds = max(0, $timer->remaining_seconds + ($secondsPassed / 2));
         }
 
         // Store previous status before any change
