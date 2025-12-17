f7c6da6ac1dd84212e2d6cdd7e15174351a24a0f
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-17 18:47:28 +0530

    m

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -894,7 +894,11 @@ class DashboardController extends Controller
 
         // Update remaining seconds if timer is running
         if ($timer->status === 'running') {
-            if (!is_null($timer->last_decrement) && $timer->updated_at->gt($timer->last_decrement)) {
+            if (
+                $timer->last_decrement &&
+                $timer->updated_at->gt($timer->last_decrement) &&
+                $timer->last_decrement->diffInSeconds($timer->updated_at) > 3
+            ) {
                 $elapsed = $timer->last_decrement->diffInSeconds($currentTime);
                 // Decrease remaining_seconds once
                 $timer->remaining_seconds = max(0, $timer->remaining_seconds - $elapsed);
