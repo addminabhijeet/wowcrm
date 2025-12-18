1301121d6d08e8c82ab452a5235e8bb8f6b26882
addminabhijeet <addmin.abhijeet@gmail.com>
2025-12-18 21:47:40 +0530

    m

--- resources/views/database/accountant.blade.php
+++ resources/views/database/accountant.blade.php
@@ -952,12 +952,12 @@
     ">
 
         <!-- SINGLE IFRAME CARD -->
-        <div style="background:#fff; padding:18px;  max-width:1010px;
+        <div style="background:#fff; padding:18px; border-radius:12px; max-width:1010px;
             box-shadow:0 4px 18px rgba(0,0,0,0.08); border:1px solid #eee;">
             <h4 style="margin:0 0 12px; font-size:17px; color:#444;">Acceptance Form</h4>
             <iframe
                 src="{{ route('pdf.acceptance') }}?${queryParams}"
-                style="width:100%; height:1270px; border:0; border-radius:10px;">
+                style="width:100%; height:1270px;">
             </iframe>
         </div>
 
