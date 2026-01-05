433fec0e7fedb7c80837b05c5fc5ed31497db594
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-05 22:49:12 +0530

    m

--- resources/views/timers/senior.blade.php
+++ resources/views/timers/senior.blade.php
@@ -57,13 +57,13 @@
                 @foreach ($timers as $timer)
                     <div class="col-xxl-3 col-md-6 user-grid-card">
                         <div class="position-relative border radius-16 overflow-hidden" style="padding-top: 30px;">
-                            <img src="{{ asset('assets/images/user-grid/user-grid-bg1.png') }}"
-                                class="w-100 object-fit-cover" alt="">
+                            {{-- <img src="{{ asset('assets/images/user-grid/user-grid-bg1.png') }}"
+                                class="w-100 object-fit-cover" alt=""> --}}
 
                             <div class="ps-16 pb-16 pe-16 text-center mt--50">
-                                {{-- <img src="{{ $timer['image'] ? asset('storage/app/public/' . $timer['image']) : asset('assets/images/user-grid/user-grid-bg1.png') }}"
+                                <img src="{{ $timer['image'] ? asset('storage/app/public/' . $timer['image']) : asset('assets/images/user-grid/user-grid-bg1.png') }}"
                                     class="border br-white border-width-2-px w-100-px h-100-px rounded-circle object-fit-cover"
-                                    alt=""> --}}
+                                    alt="">
                                 <h6 class="text-lg mb-0 mt-4">{{ $timer['name'] }}</h6>
                                 <span class="text-secondary-light mb-16">{{ $timer['email'] }}</span>
 
