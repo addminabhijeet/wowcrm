ecacc9cb0e3a823c4ea058b38eb469efcd289939
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-07 04:19:19 +0530

    m

--- resources/views/calendar/setting.blade.php
+++ resources/views/calendar/setting.blade.php
@@ -481,12 +481,12 @@
             });
         }
     </script>
-
     <script>
         document.getElementById('openCalendar').addEventListener('click', function() {
-            $('#calendarPicker').datepicker('show');
+            document.getElementById('calendarPicker').focus();
         });
     </script>
+
 
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
