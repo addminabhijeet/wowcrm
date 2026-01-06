4462dbff8456dcf12a55ffd9b412165818f6e6f9
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-07 03:22:27 +0530

    m

--- resources/views/calendar/setting.blade.php
+++ resources/views/calendar/setting.blade.php
@@ -14,6 +14,33 @@
 
 @section('content')
     <div id="pdfContent">
+        <div class="row mb-3">
+            <div class="col-md-3">
+                <select id="monthSelect" class="form-select">
+                    <option value="0">January</option>
+                    <option value="1">February</option>
+                    <option value="2">March</option>
+                    <option value="3">April</option>
+                    <option value="4">May</option>
+                    <option value="5">June</option>
+                    <option value="6">July</option>
+                    <option value="7">August</option>
+                    <option value="8">September</option>
+                    <option value="9">October</option>
+                    <option value="10">November</option>
+                    <option value="11">December</option>
+                </select>
+            </div>
+
+            <div class="col-md-3">
+                <select id="yearSelect" class="form-select">
+                    @for ($y = date('Y') - 2; $y <= date('Y') + 5; $y++)
+                        <option value="{{ $y }}">{{ $y }}</option>
+                    @endfor
+                </select>
+            </div>
+        </div>
+
         <div class="row gy-4 mt-1">
             <div class="col-12">
                 <div class="card h-100 border-0 shadow-sm radius-12">
@@ -417,5 +444,65 @@
                 });
             });
         });
+    </script>
+
+    <script>
+        const monthSelect = document.getElementById('monthSelect');
+        const yearSelect = document.getElementById('yearSelect');
+        const tableBody = document.getElementById('dateTableBody');
+
+        function generateDates() {
+            tableBody.innerHTML = '';
+
+            const month = parseInt(monthSelect.value);
+            const year = parseInt(yearSelect.value);
+
+            const daysInMonth = new Date(year, month + 1, 0).getDate();
+
+            for (let day = 17; day <= daysInMonth; day++) {
+                const fullDate = `${year}-${String(month + 1).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+
+                tableBody.innerHTML += `
+            <tr>
+                <td>${day}</td>
+                <td class="text-center">
+                    <div class="form-check d-inline-block ms-2">
+                        <input class="form-check-input holiday-checkbox"
+                               type="checkbox"
+                               data-date="${fullDate}">
+                    </div>
+                </td>
+            </tr>
+        `;
+            }
+
+            attachCheckboxEvents();
+        }
+
+        function attachCheckboxEvents() {
+            document.querySelectorAll('.holiday-checkbox').forEach(checkbox => {
+                checkbox.addEventListener('change', function() {
+                    fetch('/holiday/save', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}'
+                        },
+                        body: JSON.stringify({
+                            date: this.dataset.date,
+                            is_holiday: this.checked ? 1 : 0
+                        })
+                    });
+                });
+            });
+        }
+
+        monthSelect.addEventListener('change', generateDates);
+        yearSelect.addEventListener('change', generateDates);
+
+        // Initial load
+        monthSelect.value = new Date().getMonth();
+        yearSelect.value = new Date().getFullYear();
+        generateDates();
     </script>
 @endsection
