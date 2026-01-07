5e4492fc4158db225d0310e7faaf0e80e2f7e7f6
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-08 02:25:11 +0530

    m

--- app/Http/Controllers/GoogleSheetController.php
+++ app/Http/Controllers/GoogleSheetController.php
@@ -4132,17 +4132,24 @@ class GoogleSheetController extends Controller
 
         $data = GoogleSheetData::where(function ($q) use ($juniorPart) {
             // Check first segment is junior
-            $q->whereRaw("SUBSTRING_INDEX(created_by, ':', 1) = ?", [$juniorPart]);
+            $q->whereRaw(
+                "SUBSTRING_INDEX(created_by, ':', 1) = ?",
+                [$juniorPart]
+            );
         })
             ->where(function ($q) {
                 // Check second segment is senior (any ID or 0)
-                $q->whereRaw("SUBSTRING_INDEX(SUBSTRING_INDEX(created_by, ':', 2), ':', -1) LIKE '%|senior'");
+                $q->whereRaw(
+                    "SUBSTRING_INDEX(SUBSTRING_INDEX(created_by, ':', 2), ':', -1) LIKE '%|senior'"
+                );
             })
-            ->orderBy('Date', 'desc') // ✅ sort by Date descending
+            ->where('transfer', 0) // ✅ show only records where transfer = 0
+            ->orderBy('Date', 'desc')
             ->paginate(10);
 
         return view('database.juniorcandm', compact('data'));
     }
+
 
 
 
