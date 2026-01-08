a3a70b0d116a571148a560b089cccfe0ceb2d3cf
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-09 00:44:10 +0530

    m

--- resources/views/database/senior.blade.php
+++ resources/views/database/senior.blade.php
@@ -287,7 +287,7 @@
 
                                     {{-- Status --}}
                                     <td>
-                                        @php $exeOptions = ['Called & Mailed','Not Interested','Not Connected','Did Not Pickup','Others','Ready To Pay','VM','Busy']; @endphp
+                                        @php $exeOptions = ['Called & Mailed','Not Interested','Interested','Others','Ready To Pay','VM','Busy']; @endphp
                                         <select class="form-select dynamic-dropdown" data-key="Exe Remarks">
                                             <option value="">-- Status --</option>
                                             @foreach ($exeOptions as $option)
