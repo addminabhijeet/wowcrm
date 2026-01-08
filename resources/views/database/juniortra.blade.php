1b85b499b288ce76f02bd2ac9fb9162e69b853bb
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-08 18:48:21 +0530

    m

--- resources/views/database/juniortra.blade.php
+++ resources/views/database/juniortra.blade.php
@@ -261,9 +261,9 @@
 
                                     {{-- Remark --}}
                                     <td>
-                                        <input type="text" class="form-control remark-autocomplete" data-key="Remark"
-                                            value="{{ $row->Remark ?? '' }}" placeholder="Type remark">
+                                        <textarea class="form-control remark-autocomplete" data-key="Remark" placeholder="Type remark" rows="2">{{ $row->Remark ?? '' }}</textarea>
                                     </td>
+
                                     {{-- Status --}}
                                     <td>
                                         @php
@@ -289,7 +289,7 @@
                                         </select>
                                     </td>
 
-                                   
+
                                 </tr>
                             @endforeach
                         </tbody>
