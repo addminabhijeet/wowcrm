bc702214fd0b85de2e0054fa82f471d2bad6bfeb
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-09 00:38:03 +0530

    m

--- resources/views/database/senior.blade.php
+++ resources/views/database/senior.blade.php
@@ -81,6 +81,7 @@
                                 <th scope="col" class="text-center">Forwarded By</th>
                                 <th scope="col" class="text-center">Resume</th>
                                 <th scope="col" class="text-center">Remark</th>
+                                <th scope="col" class="text-center">Transfer Remark</th>
                                 <th scope="col" class="text-center">Status</th>
                                 @auth
                                     @if (auth()->user()->role !== 'operation')
@@ -276,6 +277,11 @@
                                     {{-- Remark --}}
                                     <td>
                                         <textarea class="form-control remark-autocomplete" data-key="Remark" placeholder="Type remark" rows="2">{{ $row->Remark ?? '' }}</textarea>
+                                    </td>
+
+                                    {{-- Remark --}}
+                                    <td>
+                                        <textarea class="form-control remark-autocomplete" data-key="TransferRemark" placeholder="Type remark" rows="2">{{ $row->TransferRemark ?? '' }}</textarea>
                                     </td>
 
 
@@ -719,65 +725,65 @@
                 let cells = `<td>—</td>`;
 
                 colKeys.forEach(k => {
-    //                 if (['Exe Remarks', 'Immigration', 'Relocation', '1st Follow Up Remarks', 'Course',
-    //                         'Time Zone', 'Qualification'
-    //                     ].includes(k)) {
-    //                     let opts = [];
-    //                     if (k === 'Qualification') opts = ['Masters', 'Masters of Science', 'Bachelors',
-    //                         'PG', 'MBA', 'PG Diploma', 'M.Tech', 'B.Tech', 'MA', 'Associate Degree',
-    //                         'Aerospace Proj. Manag.'
-    //                     ];
-    //                     if (k === 'Exe Remarks') opts = ['Called & Mailed', 'Not Interested',
-    //                         'Not Connected', 'Did Not Connect', 'Others', 'Ready To Pay', 'VM', 'Busy'
-    //                     ];
-    //                     if (k === 'Immigration') opts = ['F1 CPT', 'F1 OPT', 'STEM OPT', 'H1B', 'B2', 'B1',
-    //                         'H4', 'H4 EAD', 'GC/PR', 'GC EAD', 'USC'
-    //                     ];
-    //                     if (k === 'Relocation') opts = ['YES', 'NO'];
-    //                     if (k === '1st Follow Up Remarks') opts = ['Interested', 'Doubt need Clarification',
-    //                         'Money Issue', 'Not Interested', "Don't Call"
-    //                     ];
-    //                     if (k === 'Course') opts = ['BA', 'SAS', 'JAVA', 'QA', 'SQL', 'PYTHON', 'DOT NET'];
-    //                     if (k === 'Time Zone') opts = ['EST', 'CST', 'MST', 'PST'];
-    //                     cells +=
-    //                         `<td><select class="form-select dynamic-dropdown" data-key="${k}"><option value="" disabled selected>-- Select ${k} --</option>${opts.map(o=>`<option value="${o}">${o}</option>`).join('')}</select></td>`;
-    //                 } else if (k === 'Amount') {
-    //                     cells +=
-    //                         `<td><input type="text" class="form-control amount-input" data-key="${k}" placeholder="Amount (469)"></td>`;
-    //                 } else if (k === 'Location') {
-    //                     cells +=
-    //                         `<td><input type="text" class="form-control location-autocomplete" data-key="${k}" placeholder="Location"><span class="small-hint"></span></td>`;
-    //                 } else if (k === 'Remark') {
-    //                     cells +=
-    //                         `<td><input type="text" class="form-control Remark-autocomplete" data-key="${k}" placeholder="Remark"><span class="small-hint"></span></td>`;
-    //                 } else if (k === 'Date' || k === 'Graduation Date') {
-    //                     cells +=
-    //                         `<td><input type="text" class="form-control date-picker" data-key="${k}" placeholder="${k} (MM/DD/YYYY)"><span class="small-hint"></span></td>`;
-    //                 } else if (k === 'Phone Number') {
-    //                     cells +=
-    //                         `<td><input type="tel" class="form-control phone-input" data-key="${k}" maxlength="12" placeholder="US number"><span class="phone-hint"></span></td>`;
-    //                 } else if (k === 'Email Address') {
-    //                     cells +=
-    //                         `<td><input type="email" class="form-control email-input" data-key="${k}" placeholder="Email"><span class="small-hint"></span></td>`;
-    //                 } else if (k === 'Name') {
-    //                     cells +=
-    //                         `<td><input type="text" class="form-control name-input" data-key="${k}" placeholder="Name"><span class="small-hint"></span></td>`;
-    //                 } else if (k === 'forwardedBy') {
-    //                     cells +=
-    //                         `<td><input type="text" class="form-control forwardedBy-input" data-key="forwardedBy" placeholder="Forwarded By" readonly><span class="small-hint"></span></td>`;
-    //                 } else if (k === 'View') {
-    //                     cells += `<td>
-    //     <input type="file" accept=".pdf, .doc, .docx" class="d-none resume-input" data-key="View">
-    //     <button type="button" class="btn btn-sm btn-info upload-btn">Upload</button>
-    //     <a href="#" target="_blank" class="btn btn-sm btn-primary view-btn d-none">View File</a>
-    //     <a href="#" download class="btn btn-sm btn-secondary download-btn d-none">Download</a>
-    // </td>`;
-    //                 }
+                    //                 if (['Exe Remarks', 'Immigration', 'Relocation', '1st Follow Up Remarks', 'Course',
+                    //                         'Time Zone', 'Qualification'
+                    //                     ].includes(k)) {
+                    //                     let opts = [];
+                    //                     if (k === 'Qualification') opts = ['Masters', 'Masters of Science', 'Bachelors',
+                    //                         'PG', 'MBA', 'PG Diploma', 'M.Tech', 'B.Tech', 'MA', 'Associate Degree',
+                    //                         'Aerospace Proj. Manag.'
+                    //                     ];
+                    //                     if (k === 'Exe Remarks') opts = ['Called & Mailed', 'Not Interested',
+                    //                         'Not Connected', 'Did Not Connect', 'Others', 'Ready To Pay', 'VM', 'Busy'
+                    //                     ];
+                    //                     if (k === 'Immigration') opts = ['F1 CPT', 'F1 OPT', 'STEM OPT', 'H1B', 'B2', 'B1',
+                    //                         'H4', 'H4 EAD', 'GC/PR', 'GC EAD', 'USC'
+                    //                     ];
+                    //                     if (k === 'Relocation') opts = ['YES', 'NO'];
+                    //                     if (k === '1st Follow Up Remarks') opts = ['Interested', 'Doubt need Clarification',
+                    //                         'Money Issue', 'Not Interested', "Don't Call"
+                    //                     ];
+                    //                     if (k === 'Course') opts = ['BA', 'SAS', 'JAVA', 'QA', 'SQL', 'PYTHON', 'DOT NET'];
+                    //                     if (k === 'Time Zone') opts = ['EST', 'CST', 'MST', 'PST'];
+                    //                     cells +=
+                    //                         `<td><select class="form-select dynamic-dropdown" data-key="${k}"><option value="" disabled selected>-- Select ${k} --</option>${opts.map(o=>`<option value="${o}">${o}</option>`).join('')}</select></td>`;
+                    //                 } else if (k === 'Amount') {
+                    //                     cells +=
+                    //                         `<td><input type="text" class="form-control amount-input" data-key="${k}" placeholder="Amount (469)"></td>`;
+                    //                 } else if (k === 'Location') {
+                    //                     cells +=
+                    //                         `<td><input type="text" class="form-control location-autocomplete" data-key="${k}" placeholder="Location"><span class="small-hint"></span></td>`;
+                    //                 } else if (k === 'Remark') {
+                    //                     cells +=
+                    //                         `<td><input type="text" class="form-control Remark-autocomplete" data-key="${k}" placeholder="Remark"><span class="small-hint"></span></td>`;
+                    //                 } else if (k === 'Date' || k === 'Graduation Date') {
+                    //                     cells +=
+                    //                         `<td><input type="text" class="form-control date-picker" data-key="${k}" placeholder="${k} (MM/DD/YYYY)"><span class="small-hint"></span></td>`;
+                    //                 } else if (k === 'Phone Number') {
+                    //                     cells +=
+                    //                         `<td><input type="tel" class="form-control phone-input" data-key="${k}" maxlength="12" placeholder="US number"><span class="phone-hint"></span></td>`;
+                    //                 } else if (k === 'Email Address') {
+                    //                     cells +=
+                    //                         `<td><input type="email" class="form-control email-input" data-key="${k}" placeholder="Email"><span class="small-hint"></span></td>`;
+                    //                 } else if (k === 'Name') {
+                    //                     cells +=
+                    //                         `<td><input type="text" class="form-control name-input" data-key="${k}" placeholder="Name"><span class="small-hint"></span></td>`;
+                    //                 } else if (k === 'forwardedBy') {
+                    //                     cells +=
+                    //                         `<td><input type="text" class="form-control forwardedBy-input" data-key="forwardedBy" placeholder="Forwarded By" readonly><span class="small-hint"></span></td>`;
+                    //                 } else if (k === 'View') {
+                    //                     cells += `<td>
+                //     <input type="file" accept=".pdf, .doc, .docx" class="d-none resume-input" data-key="View">
+                //     <button type="button" class="btn btn-sm btn-info upload-btn">Upload</button>
+                //     <a href="#" target="_blank" class="btn btn-sm btn-primary view-btn d-none">View File</a>
+                //     <a href="#" download class="btn btn-sm btn-secondary download-btn d-none">Download</a>
+                // </td>`;
+                    //                 }
                 });
 
                 cells +=
                     // `<td><button class="btn btn-sm btn-success save-btn" data-id="new"><i class="fas fa-save"></i> Save</button></td>`;
-                newRow.innerHTML = cells;
+                    newRow.innerHTML = cells;
                 tableBody.appendChild(newRow);
                 applyInitialState(newRow);
             }
