e094cafe586d43d1d1eeddf4c6fb7dde5a37e916
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-11 12:42:03 +0530

    m

--- resources/views/database/seniormod.blade.php
+++ resources/views/database/seniormod.blade.php
@@ -286,7 +286,16 @@
                                             value="{{ $row->Remark ?? '' }}" placeholder="Type remark">
                                     </td>
 
+                                    {{-- TransferRemark --}}
+                                    <td>
+                                        <textarea type="text" name="TransferRemark_hidden" class="form-control transferremark-autocomplete data-field"
+                                            data-key="TransferRemark" placeholder="Type remark" rows="2">{{ $row->TransferRemark ?? '' }}</textarea>
 
+                                        <input type="hidden" name="TransferRemark"
+                                            class="form-control transferremark-autocomplete transferremark-hidden"
+                                            data-key="TransferRemark" value="{{ $row->TransferRemark ?? '' }}"
+                                            placeholder="Type TransferRemark">
+                                    </td>
 
                                     {{-- Status --}}
                                     <td>
