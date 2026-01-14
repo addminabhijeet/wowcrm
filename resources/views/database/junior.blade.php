336f4a010bece6b2abc690fe961a79dde9c8eeac
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-14 16:50:52 +0530

    m

--- resources/views/database/junior.blade.php
+++ resources/views/database/junior.blade.php
@@ -1461,7 +1461,7 @@
                                     data.data.Name ?? '';
 
                                 row.querySelector('[data-key="Date"]').value =
-                                    data.data.Name ?? '';
+                                    data.data.Date ?? '';
 
                                 row.querySelector('[data-key="Phone Number"]').value =
                                     data.data.Phone_Number ?? '';
