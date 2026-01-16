e9e7d102b98b3e7dcf2e0b3d71c28c365fce2344
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-16 21:32:33 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -216,6 +216,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard/senior/google-sheet/search-follow', [GoogleSheetController::class, 'seniorfollowSuggestions'])->name('seniorfollow.suggestions');
     Route::get('/dashboard/senior/google-sheet/search-modcandm', [GoogleSheetController::class, 'seniormodcandmSuggestions'])->name('seniormodcandm.suggestions');
     Route::get('/dashboard/senior/google-sheet/search-modcandmfollow', [GoogleSheetController::class, 'seniormodcandmfollowSuggestions'])->name('seniormodcandmfollow.suggestions');
+    Route::get('/dashboard/senior/google-sheet/seniorcandm', [GoogleSheetController::class, 'seniorcandmSuggestions'])->name('seniorcandm.suggestions');
     Route::get('/dashboard/senior/google-sheet/search', [GoogleSheetController::class, 'seniorSuggestions'])->name('senior.suggestions');
     Route::get('/dashboard/senior/google-sheet/search-paid', [GoogleSheetController::class, 'seniorpaidSuggestions'])->name('senior.suggestionspaid');
     Route::get('/dashboard/career/google-sheet/search', [GoogleSheetController::class, 'careerSuggestions'])->name('career.suggestions');
