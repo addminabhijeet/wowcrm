2c5a8ae4f3804e69e5e7823e09ad700503640251
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-19 21:27:22 +0530

    m

--- resources/views/database/junior.blade.php
+++ resources/views/database/junior.blade.php
@@ -70,14 +70,22 @@
         <div
             class="card-header border-bottom bg-base py-16 px-24 d-flex align-items-center flex-wrap gap-3 justify-content-between">
             <div class="d-flex align-items-center flex-wrap gap-3">
-                <form class="navbar-search position-relative" autocomplete="off">
+
+                <form class="navbar-search position-relative d-flex gap-2" autocomplete="off">
                     <input type="text" id="senior-search" class="bg-base h-40-px w-auto form-control"
                         placeholder="Search Name, Email, Phone">
+
+                    <input type="date" id="date-filter" class="bg-base h-40-px w-auto form-control"
+                        title="Filter by Date">
+
                     <iconify-icon icon="ion:search-outline" class="icon"></iconify-icon>
+
                     <div id="search-suggestions" class="list-group position-absolute w-100" style="z-index:1000;"></div>
                 </form>
+
             </div>
         </div>
+
         <div class="card-body p-24" id="senior-table-wrapper">
             <!-- Extra Scroll Bar Above -->
             <!-- Extra Scroll Bar Above -->
@@ -145,7 +153,8 @@
                                     {{-- Phone Number --}}
                                     <td>
                                         <input type="tel" class="form-control phone-input" data-key="Phone Number"
-                                            maxlength="14" value="{{ $row->Phone_Number ?? '' }}" placeholder="US number">
+                                            maxlength="14" value="{{ $row->Phone_Number ?? '' }}"
+                                            placeholder="US number">
                                     </td>
 
                                     {{-- Location --}}
