f034f52e37d18cae39e389d5d47bfd51864e9f8f
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-20 02:25:49 +0530

    m

--- resources/views/database/juniorcandm.blade.php
+++ resources/views/database/juniorcandm.blade.php
@@ -1039,7 +1039,6 @@
                     type: 'GET',
                     data: {
                         query,
-                        junior_user
                     },
                     success: function(res) {
                         let suggestions = '';
@@ -1062,11 +1061,9 @@
             // Click suggestion
             $(document).on('click', '#search-suggestions a', function(e) {
                 e.preventDefault();
-
                 const rowId = $(this).data('id');
                 const junior_user = $('#junior-filter').val();
                 const date = $('#date-filter').val();
-                
                 $('#senior-search').val($(this).text());
                 $('#search-suggestions').empty().hide();
 
