b9986fa916b617e2bd2ed053f033c03956f17114
addminabhijeet <addmin.abhijeet@gmail.com>
2026-01-28 15:42:19 +0530

    m

--- app/Http/Controllers/CallReportController.php
+++ app/Http/Controllers/CallReportController.php
@@ -1496,7 +1496,7 @@ class CallReportController extends Controller
 
     public function reportsender(Request $request)
     {
-        $juniorUsers = User::where('is_deleted', 0)->get();
+        $juniorUsers = User::where('is_deleted', 0)->whereIn('role',['junior','senior'])->get();
         // Pass users to the view
         return view('reports.reportsender', compact('juniorUsers'));
     }
