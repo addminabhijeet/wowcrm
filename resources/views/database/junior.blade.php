a6f79c5399a036b630b80c5bcd773dd76656415c
addminabhijeet <addmin.abhijeet@gmail.com>
2026-02-23 18:48:26 +0530

    m

--- resources/views/database/junior.blade.php
+++ resources/views/database/junior.blade.php
@@ -802,7 +802,8 @@
                             `<td><input type="text" class="form-control location-autocomplete" data-key="${k}" placeholder="Location"><span class="small-hint"></span></td>`;
                     } else if (k === 'Remark') {
                         cells +=
-                            `<td><input type="text" class="form-control remark-autocomplete" data-key="${k}" placeholder="Remark"><span class="small-hint"></span></td><td></td>`;
+                            `<td colspan="2"><input type="text" class="form-control remark-autocomplete" data-key="${k}" placeholder="Remark"><span class="small-hint"></span></td>`;
+                        return;
                     } else if (k === 'Date' || k === 'Graduation Date') {
                         cells +=
                             `<td><input type="text" class="form-control date-picker" data-key="${k}" placeholder="${k} (MM/DD/YYYY)"><span class="small-hint"></span></td>`;
