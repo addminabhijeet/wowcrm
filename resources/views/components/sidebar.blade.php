813e63012afad89c2d59e52381f6db34ac73c6f5
addminabhijeet <addmin.abhijeet@gmail.com>
2026-03-20 19:29:41 +0530

    m

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -143,7 +143,7 @@
                     <ul class="sidebar-submenu">
                         <li><a href="{{ route('google.sheet.seniortraotp') }}"><i
                                     class="ri-circle-fill circle-icon text-primary-600 w-auto"></i>IT Recruiter
-                                <br>(Called & Mailed Steam Otp)</a>
+                                <br>(Called & Mailed Steam Opt)</a>
                         </li>
                     </ul>
 
