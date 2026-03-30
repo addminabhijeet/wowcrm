77dabc60be55ebc45baa5bcb07cb62f6a7f70f72
addminabhijeet <addmin.abhijeet@gmail.com>
2026-03-30 18:20:13 +0530

    m

--- resources/views/database/seniorsearch.blade.php
+++ resources/views/database/seniorsearch.blade.php
@@ -89,7 +89,7 @@
                                     <td colspan="5" class="p-1">
 
                                         <!-- compact wrapper -->
-                                        <div class="p-1 border rounded bg-light small" style="max-width:700px;">
+                                        <div class="p-1 border rounded bg-light small" >
 
                                             <div class="row g-1 justify-content-start">
 
