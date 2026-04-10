13f7b5baa020a8a54c7ff06aafe340d401eaff84
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-10 19:52:28 +0530

    hj

--- app/Http/Controllers/CallReportController.php
+++ app/Http/Controllers/CallReportController.php
@@ -130,7 +130,6 @@ class CallReportController extends Controller
         $SfollowUpCalls = GoogleSheetData::whereRaw(
             "created_by REGEXP '^[0-9]+\\|junior:0\\|senior$'"
         )
-            ->whereRaw("created_by REGEXP '^[0-9]+\\|junior'")
             ->whereRaw("created_by NOT REGEXP '^[0-9]+\\|senior:0\\|senior$'") // ✅ ensure starts with x|junior
             ->whereDate('updated_at', $selectedDate)
             ->where('Exe_Remarks', 'Called & Mailed')
