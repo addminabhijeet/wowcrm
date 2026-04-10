c8b61fd22950cced88271884f8942170133493b4
addminabhijeet <addmin.abhijeet@gmail.com>
2026-04-10 23:53:38 +0530

    mm

--- app/Http/Controllers/GoogleSheetController.php
+++ app/Http/Controllers/GoogleSheetController.php
@@ -7756,6 +7756,21 @@ class GoogleSheetController extends Controller
 
             $user = Auth::user();
 
+
+            if (!empty($email)) {
+
+                $emailBlocked = GoogleSheetData::where('Email_Address', $email)
+                    ->where('created_by', 'like', '%accountant%')
+                    ->exists();
+
+                if ($emailBlocked) {
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'Candidate already enrolled.'
+                    ]);
+                }
+            }
+
             //  Atomic duplicate email check
             if (!empty($email)) {
                 $emailExistsForUser = GoogleSheetData::where('Email_Address', $email)
