80918b85e6e983f2f3a627ff8fd3b3827fcb3005
addminabhijeet <addmin.abhijeet@gmail.com>
2026-07-16 10:51:43 +0530

    bmn

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,13 +66,4 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
-    protected function schedule(Schedule $schedule)
-    {
-        $schedule->command('timers:decrement')
-          ->everyMinute()
-          ->withoutOverlapping()
-          ->runInBackground();
-    }
-
-
 }
