144bb8b95e113b62e6737a54b72d792a61b46937
addminabhijeet <addmin.abhijeet@gmail.com>
2026-07-21 02:39:52 +0530

    mnb

--- resources/views/user/seniorgroupmailchart.blade.php
+++ resources/views/user/seniorgroupmailchart.blade.php
@@ -4,18 +4,43 @@
 
 @php
 $timeSlots = [
-['title' => '8:00am - 9:00am', 'field' => 't8to9am'],
-['title' => '9:00am - 10:00am', 'field' => 't9to10am'],
-['title' => '10:00am - 11:00am','field' => 't10to11am'],
-['title' => '11:00am - 12:00pm','field' => 't11to12pm'],
-['title' => '12:00pm - 1:00pm', 'field' => 't12to1pm'],
-['title' => '1:00pm - 2:00pm', 'field' => 't1to2pm'],
-['title' => '2:00pm - 3:00pm', 'field' => 't2to3pm'],
-['title' => '3:00pm - 4:00pm', 'field' => 't3to4pm'],
-['title' => '4:00pm - 5:00pm', 'field' => 't4to5pm'],
-['title' => '5:00pm - 6:00pm', 'field' => 't5to6pm'],
-['title' => '6:00pm - 7:00pm', 'field' => 't6to7pm'],
-['title' => '7:00pm - 8:00pm', 'field' => 't7to8pm'],
+[
+'title' => '10:00am - 11:00am',
+'fields' => ['t8to9am', 't9to10am', 't10to11am']
+],
+[
+'title' => '11:00am - 12:00pm',
+'fields' => ['t11to12pm']
+],
+[
+'title' => '12:00pm - 1:00pm',
+'fields' => ['t12to1pm']
+],
+[
+'title' => '1:00pm - 2:00pm',
+'fields' => ['t1to2pm']
+],
+[
+'title' => '2:00pm - 3:00pm',
+'fields' => ['t2to3pm']
+],
+[
+'title' => '3:00pm - 4:00pm',
+'fields' => ['t3to4pm']
+],
+[
+'title' => '4:00pm - 5:00pm',
+'fields' => ['t4to5pm']
+],
+[
+'title' => '5:00pm - 6:00pm',
+'fields' => ['t5to6pm']
+],
+[
+// Last slot ends at 7 PM and includes both 6-7 PM & 7-8 PM counts
+'title' => '6:00pm - 7:00pm',
+'fields' => ['t6to7pm', 't7to8pm']
+],
 ];
 @endphp
 
@@ -24,10 +49,14 @@ $timeSlots = [
     @foreach($timeSlots as $slot)
 
     <div class="card mb-5">
+
         <div class="card-body" id="copySection{{ $loop->index }}">
 
             <div class="d-flex justify-content-between align-items-center mb-3">
-                <h4 class="mb-0">C&amp;M Count</h4>
+
+                <h4 class="mb-0">
+                    C&amp;M Count
+                </h4>
 
                 <button
                     type="button"
@@ -35,11 +64,17 @@ $timeSlots = [
                     onclick="copySection('copySection{{ $loop->index }}', this)">
                     Copy
                 </button>
+
             </div>
 
-            <p><strong>Date:</strong> {{ \Carbon\Carbon::today()->format('d-m-Y') }}</p>
+            <p>
+                <strong>Date:</strong>
+                {{ \Carbon\Carbon::today()->format('d-m-Y') }}
+            </p>
 
-            <p><strong>{{ $slot['title'] }}</strong></p>
+            <p>
+                <strong>{{ $slot['title'] }}</strong>
+            </p>
 
             @foreach($seniors as $senior)
 
@@ -53,8 +88,15 @@ $timeSlots = [
 
             @foreach($senior->juniors as $junior)
 
-            {{ $junior->name }} -
-            {{ $junior->{$slot['field']} ?? 0 }}
+            @php
+            $count = 0;
+
+            foreach ($slot['fields'] as $field) {
+            $count += $junior->{$field} ?? 0;
+            }
+            @endphp
+
+            {{ $junior->name }} - {{ $count }}
 
             <br>
 
@@ -67,7 +109,9 @@ $timeSlots = [
             @endif
 
             <br>
+
             xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
             <br><br>
 
             @endforeach
@@ -79,11 +123,13 @@ $timeSlots = [
             </p>
 
         </div>
+
     </div>
 
     @endforeach
 
 </div>
+
 <script>
     function copySection(sectionId, button) {
 
@@ -102,10 +148,14 @@ $timeSlots = [
             }, 1500);
 
         }).catch(function(err) {
+
             alert('Failed to copy.');
+
             console.error(err);
+
         });
 
     }
 </script>
+
 @endsection
