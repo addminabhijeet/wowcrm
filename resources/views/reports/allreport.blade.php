a923d2d64d4504792ce1d27e7e26c0471ba73d27
addminabhijeet <addmin.abhijeet@gmail.com>
2026-07-22 18:17:35 +0530

    mnb

--- resources/views/reports/allreport.blade.php
+++ resources/views/reports/allreport.blade.php
@@ -1060,7 +1060,7 @@
             clonedElement.prepend(printStyle);
 
             // ✅ Wait for a short time to ensure all assets/styles load
-            await new Promise(resolve => setTimeout(resolve, 100));
+            await new Promise(resolve => setTimeout(resolve, 10000));
 
             // ✅ Proper A4 PDF dimensions in pixels
             const a4WidthPx = 1175;
