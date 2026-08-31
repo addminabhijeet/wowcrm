162746916f6965b0904362b529c091ef6d9f30a0
Abhiappsdeveloper <abhijeetkumar9006185@gmail.com>
2026-09-01 04:45:20 +0530

    bnm

--- app/Http/Controllers/TargetAnalyticsController.php
+++ app/Http/Controllers/TargetAnalyticsController.php
@@ -120,21 +120,6 @@ class TargetAnalyticsController extends Controller
                                        ->get()
                                        ->keyBy('month');
 
-        // Batch load all achieved amounts for the year using exact REGEXP pattern
-        if ($user->role === 'junior') {
-            $pattern = "created_by REGEXP '^{$userId}\\\\|junior:[0-9]+\\\\|senior:[0-9]+\\\\|accountant(.*)?$'";
-        } else {
-            // For senior role
-            $pattern = "created_by REGEXP '^{$userId}\\\\|senior:[0-9]+\\\\|senior:[0-9]+\\\\|accountant(.*)?$'";
-        }
-
-        $achievedByMonth = GoogleSheetData::whereRaw($pattern)
-                                          ->whereYear('updated_at', $year)
-                                          ->selectRaw('MONTH(updated_at) as month, SUM(Amount) as total')
-                                          ->groupByRaw('MONTH(updated_at)')
-                                          ->get()
-                                          ->keyBy('month');
-
         for ($month = 1; $month <= 12; $month++) {
             // Get target given
             if (isset($monthlyTargets[$month])) {
@@ -143,8 +128,18 @@ class TargetAnalyticsController extends Controller
                 $targetGiven = $this->getTargetFromOldSystem($userId, $year, $month, $user);
             }
 
-            // Get target achieved from batch query
-            $targetAchieved = $achievedByMonth[$month]->total ?? 0;
+            // Get target achieved using exact REGEXP pattern from CallReportController::alljuniormonthly
+            if ($user->role === 'junior') {
+                $pattern = "created_by REGEXP '^{$userId}\\\\|junior:[0-9]+\\\\|senior:[0-9]+\\\\|accountant(.*)?$'";
+            } else {
+                // For senior role
+                $pattern = "created_by REGEXP '^{$userId}\\\\|senior:[0-9]+\\\\|senior:[0-9]+\\\\|accountant(.*)?$'";
+            }
+
+            $targetAchieved = GoogleSheetData::whereRaw($pattern)
+                                             ->whereYear('updated_at', $year)
+                                             ->whereMonth('updated_at', (int) $month)
+                                             ->sum('Amount');
 
             // Calculate variance
             $variance = $targetAchieved - $targetGiven;
